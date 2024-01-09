840a71e49c651a2df2fc4d4f9e3ec2d361ee72de
Mike Ingold <mike.ingold@gmail.com>
2024-01-09 12:47:19 -0500

    Implement hasmethod checks for passed functions

--- src/integrate.jl
+++ src/integrate.jl
@@ -2,11 +2,21 @@
 
 # Integrate f(::Point{Dim,T}) over a Segment
 function integrate(segment::Meshes.Segment{Dim,T}, f) where {Dim,T}
+	# Verify that the provided function has the correct method available
+	if !hasmethod(f, (Point{Dim,T},))
+		error("The provided Function f has no method f(::Point{Dim,T})")
+	end
+	
     return length(segment) * quadgk(t -> f(segment(t)), 0, 1)[1]
 end
 
 # Integrate f(::Point{Dim,T}) over a BezierCurve
 function integrate(curve::Meshes.BezierCurve{Dim,T,V}, f) where {Dim,T,V}
+	# Verify that the provided function has the correct method available
+	if !hasmethod(f, (Point{Dim,T},))
+		error("The provided Function f has no method f(::Point{Dim,T})")
+	end
+	
     return length(curve) * quadgk(t -> f(curve(t)), 0, 1)[1]
 end
 
@@ -14,10 +24,20 @@ end
 
 # Integrate f(::Point{Dim,T}, ::Vec{Dim,T}) over a SurfaceSegment
 function integrate(ss::SurfaceSegment{Dim,T}, f) where {Dim,T}
+	# Verify that the provided function has the correct method available
+	if !hasmethod(f, (Point{Dim,T}, Vec{Dim,T}))
+		error("The provided Function f has no method f(::Point{Dim,T})")
+	end
+	
     return length(ss.segment) * quadgk(t -> f(ss.segment(t),ss.normal), 0, 1)[1]
 end
 
 # Integrate f(::Point{Dim,T}, ::Vec{Dim,T}) over a SurfaceTrajectory
 function integrate(traj::SurfaceTrajectory, f)
+	# Verify that the provided function has the correct method available
+	if !hasmethod(f, (Point{Dim,T}, Vec{Dim,T}))
+		error("The provided Function f has no method f(::Point{Dim,T})")
+	end
+	
     return sum(ss -> integrate(ss,f), traj.path)
 end
