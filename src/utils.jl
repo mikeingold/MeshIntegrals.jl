8246f3e1f9ba26d73288768d423f7b2de71e3e3f
Mike Ingold <mike.ingold@gmail.com>
2024-01-08 21:07:33 -0500

    Fix equation

--- src/utils.jl
+++ src/utils.jl
@@ -1,18 +1,16 @@
 # Get the derivative vector of a Bezier Curve at parametric t
 #   Ref https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Derivative
 function derivative(bz::Meshes.BezierCurve{Dim,T,V}, t) where {Dim,T,V}
-	P = bz.controls
-	N = length(bz.controls)
-	
-	# Binomial coefficient
-	bc(n,i) = factorial(n) / (factorial(i)*factorial(n-i))
+    # Let N be the order of the Bezier curve (number of control points minus 1)
+    P = bz.controls
+	N = length(P) - 1
 	
 	# Bernstein basis polynomials
-	b(i,n) = t -> bc(n,i) * t^i * (1-t)^(n-i)
+	b(i,n) = t -> binomial(n,i) * t^i * (1-t)^(n-i)
 	
 	# Term to be summed
 	sigma(i) = b(i,N-1)(t) .* (P[i+2] - P[i+1])
 	
 	# Derivative of curve
-	return N .* sum(sigma, 0:(N-1))
+	return N .* sum(sigma, 0:order-1)
 end
