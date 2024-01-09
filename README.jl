221042de07fe2820211d87c9193df5fec458b943
Mike Ingold <mike.ingold@gmail.com>
2024-01-08 19:24:10 -0500

    Generate basic README

---
+++ README.jl
@@ -0,0 +1,5 @@
+# LineIntegrals.jl
+
+The goal of this project is to provide a simple interface for computing line integrals
+using geometric representations from [**Meshes.jl**](https://github.com/JuliaGeometry/Meshes.jl).
+
