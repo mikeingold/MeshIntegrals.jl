d5f9afee075aa4afbec654820c5fb043c8281d6e
Mike Ingold <mike.ingold@gmail.com>
2024-01-10 21:45:27 -0500

    Add abstract type, docs

--- src/structs.jl
+++ src/structs.jl
@@ -1,14 +1,32 @@
-struct SurfaceSegment{Dim,T}
+abstract type SurfacePathElement{Dim,T} end
+
+"""
+    SufacePathSegment
+
+This represents a line segment that traverses a surface in space, defined by a
+segment with start and end points and a vector pointing in the direction of
+the local surface normal.
+"""
+struct SurfacePathSegment{Dim,T}
     segment::Meshes.Segment{Dim,T}
     normal::Meshes.Vec{Dim,T}
 end
 
+# struct SurfacePathBezierCurve{Dim,T} <: SurfacePathElement{Dim,T} end
+
+"""
+    SurfaceTrajectory
+
+This represents a path that traverses a surface in space, where the path is
+defined by an ordered collection of SurfacePathElements.
+"""
 struct SurfaceTrajectory{Dim,T}
-    path::Vector{SurfaceSegment{Dim,T}}
+    path::Vector{SurfacePathElement{Dim,T}}
 end
 
+# TODO abstract this to enable different types
 function SurfaceTrajectory(segs, norms)
     # Convert all corresponding (Segments, Normals) into SurfaceSegments
-    path = map(SurfaceSegment, Iterators.zip(segs,norms))
+    path = map(SurfacePathSegment, Iterators.zip(segs,norms))
     return SurfaceTrajectory(path)
 end
