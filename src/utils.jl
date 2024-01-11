0a3a85d194da63f8f646b1090d37dc67e0b44df6
Mike Ingold <mike.ingold@gmail.com>
2024-01-10 21:34:10 -0500

    Renaming and code improvement

--- src/utils.jl
+++ src/utils.jl
@@ -1,23 +1,40 @@
-# Get the derivative vector of a Bezier Curve at parametric t
-#   Ref https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Derivative
+"""
+    derivative(b::BezierCurve, t)
+
+Determine the vector derivative of a Bezier curve `b` for the point on the
+curve parameterized by value `t`.
+"""
 function derivative(bz::Meshes.BezierCurve{Dim,T,V}, t) where {Dim,T,V}
-    # Let N be the order of the Bezier curve (number of control points minus 1)
+    # Parameter t restricted to domain [0,1] by definition
+    if t < 0 || t > 1
+        throw(DomainError(t, "b(t) is not defined for t outside [0, 1]."))
+    end
+	
+	# Generator for Bernstein polynomial functions
+	B(i,n) = t -> binomial(n,i) * t^i * (1-t)^(n-i)
+	
+	# Derivative = N Σ_{i=0}^{N-1} sigma(i)
+    #   P indices adjusted for Julia 1-based array indexing
+    N = degree(bz)
     P = bz.controls
-	N = length(P) - 1
-	
-	# Bernstein basis polynomials
-	b(i,n) = t -> binomial(n,i) * t^i * (1-t)^(n-i)
-	
-	# Term to be summed (P adjusted for 1-based array access)
-	sigma(i) = b(i,N-1)(t) .* (P[(i+1)+1] - P[(i)+1])
-	
-	# Derivative of curve
-	return N .* sum(sigma, 0:N-1)
+    sigma(i) = B(i,N-1)(t) .* (P[(i+1)+1] - P[(i)+1])
+	return N .* sum(sigma, 0:N-1)    # ::Vec{Dim,T}
 end
 
-# Unit vector pointing in forward tangent direction of a curve at parametric t
-function direction(bz::Meshes.BezierCurve{Dim,T,V}, t) where {Dim,T,V}
+"""
+    unitdirection(b::BezierCurve, t)
+
+Determine a unit vector pointing in the forward (t+) direction of a Bezier
+curve `b` for a point on the curve parameterized by value `t`.
+"""
+function unitdirection(bz::Meshes.BezierCurve{Dim,T,V}, t) where {Dim,T,V}
+    # Parameter t restricted to domain [0,1] by definition
+    if t < 0 || t > 1
+        throw(DomainError(t, "b(t) is not defined for t outside [0, 1]."))
+    end
+
+    # Normalize the derivative of the curve
     u = derivative(bz,t)
     LinearAlgebra.normalize!(u)
-    return u
+    return u    # ::Vec{Dim,T}
 end
