5c0c6a4b249a607332f9f6a8a087c0875534c202
Michael Ingold <mike.ingold@gmail.com>
2024-01-17 07:08:48 -0500

    Add tests for DynamicQuantities

--- test/runtests.jl
+++ test/runtests.jl
@@ -38,6 +38,7 @@ using Test
         @test quadgk(f, rect_traj_rope)[1] ≈ 4sqrt(2)               # Meshes.Rope
         @test isapprox(quadgk(f, unit_circle)[1], 2pi; atol=0.15)   # Meshes.BezierCurve
         @test quadgk(f, pt_e, pt_n, pt_w, pt_s, pt_e)[1] ≈ 4sqrt(2)    # Varargs of Meshes.Point
+        @test quadgk(t -> exp(-t), 0, 10, 100)[1] ≈ 1    # Verify compatibility with generic Vararg{T} method
     end
 
     @testset "Caught Errors" begin
@@ -69,12 +70,15 @@ using Test
     end
 end
 
-@testset "Integrate Unitful" begin
+@testset "Integrate with Unitful" begin
+    m = Unitful.m
+    Ω = Unitful.Ω
+
     # Points on unit circle at axes
-    pt_e = Point( 1.0u"m",  0.0u"m", 0.0u"m")
-    pt_n = Point( 0.0u"m",  1.0u"m", 0.0u"m")
-    pt_w = Point(-1.0u"m",  0.0u"m", 0.0u"m")
-    pt_s = Point( 0.0u"m", -1.0u"m", 0.0u"m")
+    pt_e = Point( 1.0m,  0.0m, 0.0m)
+    pt_n = Point( 0.0m,  1.0m, 0.0m)
+    pt_w = Point(-1.0m,  0.0m, 0.0m)
+    pt_s = Point( 0.0m, -1.0m, 0.0m)
 
     # Line segments oriented CCW between points
     seg_ne = Segment(pt_e, pt_n)
@@ -89,24 +93,69 @@ end
 
     # Approximately circular trajectory CCW around the unit-meter circle
     unit_circle = BezierCurve(
-        [Point(cos(t)*u"m", sin(t)*u"m", 0.0u"m") for t in range(0, 2pi, length=361)]
+        [Point(cos(t)*m, sin(t)*m, 0.0m) for t in range(0, 2pi, length=361)]
     )
 
     @testset "Scalar-Valued Functions" begin
-        f(::Point{Dim,T}) where {Dim,T} = 1.0u"Ω/m"
-        @test integral(f, seg_ne) ≈ sqrt(2)*u"Ω"                            # Meshes.Segment
-        @test integral(f, rect_traj_segs) ≈ 4sqrt(2)*u"Ω"                   # Vector{::Meshes.Segment}
-        @test integral(f, rect_traj_ring) ≈ 4sqrt(2)*u"Ω"                   # Meshes.Ring
-        @test integral(f, rect_traj_rope) ≈ 4sqrt(2)*u"Ω"                   # Meshes.Rope
-        @test isapprox(integral(f, unit_circle), 2π*u"Ω"; atol=0.15u"Ω")    # Meshes.BezierCurve
+        f(::Point{Dim,T}) where {Dim,T} = 1.0Ω/m
+        @test integral(f, seg_ne) ≈ sqrt(2)*Ω                         # Meshes.Segment
+        @test integral(f, rect_traj_segs) ≈ 4sqrt(2)*Ω                # Vector{::Meshes.Segment}
+        @test integral(f, rect_traj_ring) ≈ 4sqrt(2)*Ω                # Meshes.Ring
+        @test integral(f, rect_traj_rope) ≈ 4sqrt(2)*Ω                # Meshes.Rope
+        @test isapprox(integral(f, unit_circle), 2π*Ω; atol=0.15Ω)    # Meshes.BezierCurve
     end
 
     @testset "Vector-Valued Functions" begin
-        f(::Point{Dim,T}) where {Dim,T} = [1.0u"Ω/m", 1.0u"Ω/m", 1.0u"Ω/m"]
-        @test integral(f, seg_ne) ≈ [sqrt(2), sqrt(2), sqrt(2)] .* u"Ω"                  # Meshes.Segment
-        @test integral(f, rect_traj_segs)  ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* u"Ω"    # Vector{::Meshes.Segment}
-        @test integral(f, rect_traj_ring) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* u"Ω"     # Meshes.Ring
-        @test integral(f, rect_traj_rope) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* u"Ω"     # Meshes.Rope
-        @test isapprox(integral(f, unit_circle), [2π, 2π, 2π] .* u"Ω"; atol=0.15u"Ω")    # Meshes.BezierCurve
+        f(::Point{Dim,T}) where {Dim,T} = [1.0Ω/m, 1.0Ω/m, 1.0Ω/m]
+        @test integral(f, seg_ne) ≈ [sqrt(2), sqrt(2), sqrt(2)] .* Ω                  # Meshes.Segment
+        @test integral(f, rect_traj_segs)  ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω    # Vector{::Meshes.Segment}
+        @test integral(f, rect_traj_ring) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Ring
+        @test integral(f, rect_traj_rope) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Rope
+        @test isapprox(integral(f, unit_circle), [2π, 2π, 2π] .* Ω; atol=0.15Ω)    # Meshes.BezierCurve
+    end
+end
+
+@testset "Integrate with DynamicQuantities" begin
+    m = DynamicQuantities.m
+    Ω = DynamicQuantities.Ω
+
+    # Points on unit circle at axes
+    pt_e = Point( 1.0m,  0.0m, 0.0m)
+    pt_n = Point( 0.0m,  1.0m, 0.0m)
+    pt_w = Point(-1.0m,  0.0m, 0.0m)
+    pt_s = Point( 0.0m, -1.0m, 0.0m)
+
+    # Line segments oriented CCW between points
+    seg_ne = Segment(pt_e, pt_n)
+    seg_nw = Segment(pt_n, pt_w)
+    seg_sw = Segment(pt_w, pt_s)
+    seg_se = Segment(pt_s, pt_e)
+
+    # Rectangular trajectory CCW around the four points
+    rect_traj_segs = [seg_ne, seg_nw, seg_sw, seg_se]
+    rect_traj_ring = Ring(pt_e, pt_n, pt_w, pt_s)
+    rect_traj_rope = Rope(pt_e, pt_n, pt_w, pt_s, pt_e)
+
+    # Approximately circular trajectory CCW around the unit-meter circle
+    unit_circle = BezierCurve(
+        [Point(cos(t)*m, sin(t)*m, 0.0m) for t in range(0, 2pi, length=361)]
+    )
+
+    @testset "Scalar-Valued Functions" begin
+        f(::Point{Dim,T}) where {Dim,T} = 1.0Ω/m
+        @test integral(f, seg_ne) ≈ sqrt(2)*Ω                         # Meshes.Segment
+        @test integral(f, rect_traj_segs) ≈ 4sqrt(2)*Ω                # Vector{::Meshes.Segment}
+        @test integral(f, rect_traj_ring) ≈ 4sqrt(2)*Ω                # Meshes.Ring
+        @test integral(f, rect_traj_rope) ≈ 4sqrt(2)*Ω                # Meshes.Rope
+        @test isapprox(integral(f, unit_circle), 2π*Ω; atol=0.15Ω)    # Meshes.BezierCurve
+    end
+
+    @testset "Vector-Valued Functions" begin
+        f(::Point{Dim,T}) where {Dim,T} = [1.0Ω/m, 1.0Ω/m, 1.0Ω/m]
+        @test integral(f, seg_ne) ≈ [sqrt(2), sqrt(2), sqrt(2)] .* Ω                  # Meshes.Segment
+        @test integral(f, rect_traj_segs)  ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω    # Vector{::Meshes.Segment}
+        @test integral(f, rect_traj_ring) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Ring
+        @test integral(f, rect_traj_rope) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Rope
+        @test isapprox(integral(f, unit_circle), [2π, 2π, 2π] .* Ω; atol=0.15Ω)    # Meshes.BezierCurve
     end
 end
