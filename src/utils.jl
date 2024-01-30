4f711152716ac3c93d8c3bbf2b217f7d3df8494a
Michael Ingold <mike.ingold@gmail.com>
2024-01-29 21:07:35 -0500

    Update to prevent forced BigFloat outputs

--- src/utils.jl
+++ src/utils.jl
@@ -10,13 +10,20 @@ function derivative(bz::Meshes.BezierCurve{Dim,T,V}, t) where {Dim,T,V}
         throw(DomainError(t, "b(t) is not defined for t outside [0, 1]."))
     end
 
+    # Aliases
+    P = bz.controls
+    N = degree(bz)
+
+    # Ensure that this implementation is tractible: limited by ability to calculate
+    #   binomial(N, N/2) without overflow. It's possible to extend this range by
+    #   converting N to a BigInt, but this results in always returning BigFloat's.
+    N <= 1028 || error("This algorithm overflows for curves with ⪆1000 control points.")
+
     # Generator for Bernstein polynomial functions
-    B(i,n) = t -> binomial(BigInt(n),i) * t^i * (1-t)^(n-i)
+    B(i,n) = t -> binomial(Int128(n),i) * t^i * (1-t)^(n-i)
 
     # Derivative = N Σ_{i=0}^{N-1} sigma(i)
     #   P indices adjusted for Julia 1-based array indexing
-    N = degree(bz)
-    P = bz.controls
     sigma(i) = B(i,N-1)(t) .* (P[(i+1)+1] - P[(i)+1])
     return N .* sum(sigma, 0:N-1)    # ::Vec{Dim,T}
 end
