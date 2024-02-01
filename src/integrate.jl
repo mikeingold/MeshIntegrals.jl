b18184dc03cb930f342cc1de0f976304245476f5
Michael Ingold <mike.ingold@gmail.com>
2024-01-31 20:51:13 -0500

    Remove caching and add Bezier algorithm choices

--- src/integrate.jl
+++ src/integrate.jl
@@ -17,7 +17,7 @@ function integral end
 
 
 ################################################################################
-#                                  HELPERS
+#                              integral METHODS
 ################################################################################
 
 # Validate that f has a method defined for f(::Point)
@@ -29,28 +29,7 @@ function integral end
     return nothing
 end
 
-# Pre-calculate common node/weight settings
-_cached_gausslegendre = Dict{Int64, Tuple{Vector{Float64},Vector{Float64}}}(
-      10 => gausslegendre(10),
-     100 => gausslegendre(100),
-    1000 => gausslegendre(1000),
-)
-
-function _gausslegendre(n::Int64)
-    if haskey(_cached_gausslegendre, n)
-        return _cached_gausslegendre[n]
-    else
-        return gausslegendre(n)
-    end
-end
-
-################################################################################
-#                              integral METHODS
-################################################################################
-
 # Integrate f(::Point{Dim,T}) over a Segment
-#   Allocations:
-#     gausslegendre: 2n * sizeof(Float64)
 function integral(
     f::F,
     segment::Meshes.Segment{Dim,T};
@@ -60,48 +39,54 @@ function integral(
     _validate_integrand(f,Dim,T)
 
     # Compute Gauss-Legendre nodes/weights for x in interval [-1,1]
-    xs, ws = _gausslegendre(n)
+    xs, ws = gausslegendre(n)
 
     # Change of variables: x [-1,1] ↦ t [0,1]
     t(x) = 0.5x + 0.5
-
-    # Wrapper function such that fx(x) ↦ f(segment(t))
-    fx(x) = f(segment(t(x)))
+    point(x) = curve(t(x), alg)
 
     # Integrate f along the line and apply a domain-correction factor for [-1,1] ↦ [0, length]
-    return 0.5 * length(segment) * sum(w .* fx(x) for (w,x) in zip(ws, xs))
+    return 0.5 * length(segment) * sum(w .* f(point(x)) for (w,x) in zip(ws, xs))
 end
 
-# Integrate f(::Point{Dim,T}) over a BezierCurve
-#   Allocations:
-#     gausslegendre: 2n * sizeof(Float64)
+"""
+    integral(f, curve; n=100, alg=Meshes.Horner())
+
+Numerically integrate a given function `f(::Point)` along a BezierCurve using
+a Gauss-Legendre quadrature of order `n`.
+
+So long as `f` can be well-approximated by a polynomial of order `2n-1`, this
+method should yield results with 16-digit accuracy in O(n) time. If `f` is know
+to have some periodic content then `n` should (at a minimum) be greater than
+the expected number of periods, e.g. `length(geometry)/lambda`.
+
+By default this uses Horner's method to improve performance when parameterizing
+the `curve` at the expense of a small loss of precision. Additional accuracy
+can be obtained by specifying the use of DeCasteljau's algorithm instead with
+`alg=Meshes.DeCasteljau()` but can come at a steep cost in memory allocations,
+especially for curves with a large number of control points.
+"""
 function integral(
     f::F,
     curve::Meshes.BezierCurve{Dim,T,V};
-    n::Int64=100
+    n::Int64=100,
+    alg::Meshes.BezierEvalMethod=Meshes.Horner()
 ) where {F<:Function, Dim, T, V}
     # Validate the provided integrand function
     _validate_integrand(f,Dim,T)
 
     # Compute Gauss-Legendre nodes/weights for x in interval [-1,1]
-    xs, ws = _gausslegendre(n)
+    xs, ws = gausslegendre(n)
 
     # Change of variables: x [-1,1] ↦ t [0,1]
     t(x) = 0.5x + 0.5
-
-    # Wrapper function such that fx(x) ↦ f(segment(t))
-    fx(x) = f(curve(t(x)))
+    point(x) = curve(t(x), alg)
 
     # Integrate f along the line and apply a domain-correction factor for [-1,1] ↦ [0, length]
-    return 0.5 * length(curve) * mapreduce((w,x) -> w .* fx(x), +, ws, xs)
-    #return 0.5 * length(curve) * sum(w .* fx(x) for (w,x) in zip(ws, xs))
+    return 0.5 * length(curve) * sum(w .* f(point(x)) for (w,x) in zip(ws, xs))
 end
 
 # Integrate f(::Point{Dim,T}) over a Rope (an open Chain)
-#   Allocations:
-#     segments(rope)
-#     integral(f, segment)
-# TODO implement internal use _in-place methods with cached memory for performance
 function integral(
     f::F,
     rope::Meshes.Rope{Dim,T};
