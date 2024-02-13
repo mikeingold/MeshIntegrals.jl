26a9e2adb049b6b3b9ecc0f0ef5a7c102b3bb7ad
Michael Ingold <mike.ingold@gmail.com>
2024-02-13 17:45:58 -0500

    Remove obsoleted tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
-#using DynamicQuantities
 using MeshIntegrals
 using Meshes
-#using Unitful
 using Test
+
+#using DynamicQuantities
+#using Unitful
 
 ################################################################################
 #                                Line Integrals
@@ -121,110 +122,3 @@ using Test
         @test lineintegral(fc, unit_circle_complex, GaussKronrod()) ≈ 2π*im
     end
 end
-
-#= temp disabled
-################################################################################
-#                             Tests -- Unitful.jl
-################################################################################
-
-@testset "Integrate with Unitful.jl" begin
-    m = Unitful.m
-    Ω = Unitful.Ω
-
-    # Points on unit circle at axes
-    pt_e = Point( 1.0m,  0.0m, 0.0m)
-    pt_n = Point( 0.0m,  1.0m, 0.0m)
-    pt_w = Point(-1.0m,  0.0m, 0.0m)
-    pt_s = Point( 0.0m, -1.0m, 0.0m)
-
-    # Line segments oriented CCW between points
-    seg_ne = Segment(pt_e, pt_n)
-    seg_nw = Segment(pt_n, pt_w)
-    seg_sw = Segment(pt_w, pt_s)
-    seg_se = Segment(pt_s, pt_e)
-
-    # Rectangular trajectory CCW around the four points
-    rect_traj_segs = [seg_ne, seg_nw, seg_sw, seg_se]
-    ring_rect = Ring(pt_e, pt_n, pt_w, pt_s)
-    rope_rect = Rope(pt_e, pt_n, pt_w, pt_s, pt_e)
-
-    # Approximately circular trajectory CCW around the unit-meter circle
-    unit_circle = BezierCurve(
-        [Point(cos(t)*m, sin(t)*m, 0.0m) for t in range(0, 2pi, length=361)]
-    )
-
-    @testset "Scalar-Valued Functions" begin
-        f(::Point{Dim,T}) where {Dim,T} = 1.0Ω/m
-        @test lineintegral(f, seg_ne) ≈ sqrt(2)*Ω                         # Meshes.Segment
-        @test lineintegral(f, rect_traj_segs) ≈ 4sqrt(2)*Ω                # Vector{::Meshes.Segment}
-        @test lineintegral(f, ring_rect) ≈ 4sqrt(2)*Ω                # Meshes.Ring
-        @test lineintegral(f, rope_rect) ≈ 4sqrt(2)*Ω                # Meshes.Rope
-        @test isapprox(lineintegral(f, unit_circle), 2π*Ω; atol=0.15Ω)    # Meshes.BezierCurve
-    end
-
-    @testset "Vector-Valued Functions" begin
-        f(::Point{Dim,T}) where {Dim,T} = [1.0Ω/m, 1.0Ω/m, 1.0Ω/m]
-        @test lineintegral(f, seg_ne) ≈ [sqrt(2), sqrt(2), sqrt(2)] .* Ω                  # Meshes.Segment
-        @test lineintegral(f, rect_traj_segs)  ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω    # Vector{::Meshes.Segment}
-        @test lineintegral(f, ring_rect) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Ring
-        @test lineintegral(f, rope_rect) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Rope
-        @test isapprox(lineintegral(f, unit_circle), [2π, 2π, 2π] .* Ω; atol=0.15Ω)    # Meshes.BezierCurve
-    end
-end
-
-################################################################################
-#                             Tests -- DynamicQuantities.jl
-################################################################################
-
-# TODO once these tests work identically to Unitful tests, consolidate them into
-#   a single abstracted test loop
-# for Package in (Unitful, DynamicQuantities)
-#   @testset "Integrate with $Package"
-
-@testset "Integrate with DynamicQuantities.jl" begin
-    m = DynamicQuantities.m
-    Ω = DynamicQuantities.Ω
-
-    # Points on unit circle at axes
-    pt_e = Point( 1.0m,  0.0m, 0.0m)
-    pt_n = Point( 0.0m,  1.0m, 0.0m)
-    pt_w = Point(-1.0m,  0.0m, 0.0m)
-    pt_s = Point( 0.0m, -1.0m, 0.0m)
-
-    # Line segments oriented CCW between points
-    seg_ne = Segment(pt_e, pt_n)
-    seg_nw = Segment(pt_n, pt_w)
-    seg_sw = Segment(pt_w, pt_s)
-    seg_se = Segment(pt_s, pt_e)
-
-    # Rectangular trajectory CCW around the four points
-    rect_traj_segs = [seg_ne, seg_nw, seg_sw, seg_se]
-    ring_rect = Ring(pt_e, pt_n, pt_w, pt_s)
-    rope_rect = Rope(pt_e, pt_n, pt_w, pt_s, pt_e)
-
-    # Approximately circular trajectory CCW around the unit-meter circle
-    unit_circle = BezierCurve(
-        [Point(cos(t)*m, sin(t)*m, 0.0m) for t in range(0, 2pi, length=361)]
-    )
-
-    @testset "Scalar-Valued Functions" begin
-        f(::Point{Dim,T}) where {Dim,T} = 1.0Ω/m
-        @test lineintegral(f, seg_ne) ≈ sqrt(2)*Ω                         # Meshes.Segment
-        @test lineintegral(f, rect_traj_segs) ≈ 4sqrt(2)*Ω                # Vector{::Meshes.Segment}
-        @test lineintegral(f, ring_rect) ≈ 4sqrt(2)*Ω                # Meshes.Ring
-        @test lineintegral(f, rope_rect) ≈ 4sqrt(2)*Ω                # Meshes.Rope
-        @test isapprox(lineintegral(f, unit_circle), 2π*Ω; atol=0.15)    # Meshes.BezierCurve
-              # TODO change 0.15 => 0.15Ω once DynamicQuantities PR approved
-    end
-
-    @testset "Vector-Valued Functions" begin
-        f(::Point{Dim,T}) where {Dim,T} = [1.0Ω/m, 1.0Ω/m, 1.0Ω/m]
-        @test all(isapprox.(lineintegral(f, seg_ne), sqrt(2)*Ω))                # Meshes.Segment
-        @test all(isapprox.(lineintegral(f, rect_traj_segs), 4sqrt(2)*Ω))       # Vector{::Meshes.Segment}
-        @test all(isapprox.(lineintegral(f, ring_rect), 4sqrt(2)*Ω))       # Meshes.Ring
-        @test all(isapprox.(lineintegral(f, rope_rect), 4sqrt(2)*Ω))       # Meshes.Rope
-        @test all(isapprox.(lineintegral(f, unit_circle), 2π*Ω; atol=0.15))    # Meshes.BezierCurve
-             # TODO change 0.15 => 0.15Ω once DynamicQuantities PR approved
-    end
-end
-=#
