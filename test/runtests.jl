dfe81ed55f8b1d7e830cc90da8f0fd2577c4a760
Michael Ingold <mike.ingold@gmail.com>
2024-02-13 17:20:57 -0500

    Add tests to ensure invalid methods not defined

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,39 +9,68 @@ using Test
 ################################################################################
 
 @testset "Integrals" begin
+    # Spatial descriptors
+    origin3d = Point(0.0, 0.0, 0.0)
+    origin2d = Point(0.0, 0.0)
+    ẑ = Vec(0.0, 0.0, 1.0)
+
     # Points on unit circle at axes
     pt_e = Point( 1.0,  0.0, 0.0)
     pt_n = Point( 0.0,  1.0, 0.0)
     pt_w = Point(-1.0,  0.0, 0.0)
     pt_s = Point( 0.0, -1.0, 0.0)
-    origin = Point(0.0, 0.0, 0.0)
-    ẑ = Vec(0.0, 0.0, 1.0)
 
-    # Line segments oriented CCW between points
+    # Line segments/paths oriented CCW between points
     seg_ne = Segment(pt_e, pt_n)
-    seg_nw = Segment(pt_n, pt_w)
-    seg_sw = Segment(pt_w, pt_s)
-    seg_se = Segment(pt_s, pt_e)
-
-    # Rectangular trajectory CCW around the four points
-    rect_traj_ring = Ring(pt_e, pt_n, pt_w, pt_s)
-    rect_traj_rope = Rope(pt_e, pt_n, pt_w, pt_s, pt_e)
+    #seg_nw = Segment(pt_n, pt_w)
+    #seg_sw = Segment(pt_w, pt_s)
+    #seg_se = Segment(pt_s, pt_e)
+    line_ne = Line(pt_e, pt_n)
+    ring_rect = Ring(pt_e, pt_n, pt_w, pt_s)
+    rope_rect = Rope(pt_e, pt_n, pt_w, pt_s, pt_e)
 
     # Approximately circular trajectory CCW around the unit circle
-    unit_bezier = BezierCurve(
-        [Point(cos(t), sin(t), 0.0) for t in range(0, 2pi, length=361)]
-    )
+    bezier = BezierCurve([Point(cos(t), sin(t), 0.0) for t in range(0, 2pi, length=361)])
 
-    # Triangle on upper-half-plane
+    # Test Geometries
+    ball2d = Ball(origin2d, 2.0)
+    ball3d = Ball(origin3d, 2.0)
+    box1d = Box(Point(-1.0), Point(1.0))
+    box2d = Box(Point(-1.0,-1.0), Point(1.0,1.0))
+    box3d = Box(Point(-1.0,-1.0,-1.0), Point(1.0,1.0,1.0))
+    circle = Circle(Plane(origin3d,ẑ), 2.0)
+    disk = Disk(Plane(origin3d,ẑ), 2.0)
+    sphere2d = Sphere(origin2d, 2.0)
+    sphere3d = Sphere(origin3d, 2.0)
     triangle = Ngon(pt_e, pt_n, pt_w)
 
-    # Unit circle/disk
-    circle = Circle(Plane(origin,ẑ), 2.0)
-    disk = Disk(Plane(origin,ẑ), 2.0)
-    sphere2d = Sphere(Point(0.0,0.0), 2.0)
+    @testset "Errors Expected on Invalid Methods" begin
+        f(::Point) = 1.0
 
-    # 2D Box on [-1,1]^2
-    box2d = Box(Point(-1.0,-1.0), Point(1.0,1.0))
+        # lineintegrals should fail on these geometries with paramdims ≠ 1
+        @test_throws MethodError lineintegral(f, ball2d)      # Ball{2,T}
+        @test_throws MethodError lineintegral(f, ball3d)      # Ball{3,T}
+        @test_throws MethodError lineintegral(f, box2d)       # Box{2,T}
+        @test_throws MethodError lineintegral(f, box3d)       # Box{3,T}
+        # CylinderSurface
+        @test_throws MethodError lineintegral(f, disk)        # Disk
+        @test_throws MethodError lineintegral(f, triangle)    # Ngon{3,Dim,T}
+        # ParaboloidSurface
+        @test_throws MethodError lineintegral(f, sphere3d)    # Sphere{3,T}
+        # Torus
+
+        # surfaceintegrals should fail on these geometries with paramdims ≠ 1
+        @test_throws MethodError lineintegral(f, bezier)       # BezierCurve
+        @test_throws MethodError lineintegral(f, ball3d)       # Ball{3,T}
+        @test_throws MethodError lineintegral(f, box1d)        # Box{1,T}
+        @test_throws MethodError lineintegral(f, box3d)        # Box{3,T}
+        @test_throws MethodError lineintegral(f, circle)       # Circle
+        @test_throws MethodError lineintegral(f, line_ne)      # Line
+        @test_throws MethodError lineintegral(f, ring_rect)    # Ring
+        @test_throws MethodError lineintegral(f, rope_rect)    # Rope
+        @test_throws MethodError lineintegral(f, seg_ne)       # Segment
+        @test_throws MethodError lineintegral(f, sphere2d)     # Sphere{2,T}
+    end
 
     for (name,rule) in [("Gauss-Legendre",GaussLegendre(10_000)), ("Gauss-Kronrod",GaussKronrod())]
         @testset "$name" begin
@@ -49,42 +78,32 @@ using Test
                 f(::Point) = 1.0
                 # Line Integrals
                 @test lineintegral(f, seg_ne, rule) ≈ sqrt(2)                         # Meshes.Segment
-                @test lineintegral(f, rect_traj_ring, rule) ≈ 4sqrt(2)                # Meshes.Ring
-                @test lineintegral(f, rect_traj_rope, rule) ≈ 4sqrt(2)                # Meshes.Rope
-                @test lineintegral(f, unit_bezier, rule) ≈ length(unit_bezier)        # Meshes.BezierCurve
+                @test lineintegral(f, ring_rect, rule) ≈ 4sqrt(2)                # Meshes.Ring
+                @test lineintegral(f, rope_rect, rule) ≈ 4sqrt(2)                # Meshes.Rope
+                @test lineintegral(f, bezier, rule) ≈ length(bezier)        # Meshes.BezierCurve
                 @test lineintegral(f, circle, rule) ≈ length(circle)                  # Meshes.Circle
-                @test lineintegral(f, disk, rule) ≈ length(circle)                    # Meshes.Disk
                 @test lineintegral(f, sphere2d, rule) ≈ length(sphere2d)              # Meshes.Sphere{2,T}
-                @test lineintegral(f, triangle, rule) ≈ 2 + 2sqrt(2)                  # Meshes.Triangle
-                @test lineintegral(f, box2d, rule) ≈ 8.0                              # Meshes.Box{2,T}
 
                 # Surface Integrals
-                @test isapprox(surfaceintegral(f, triangle, rule), 1.0; rtol=1e-3)    # Meshes.Triangle
-                @test isapprox(surfaceintegral(f, box2d, rule), 4.0; rtol=1e-3)       # Meshes.Box{2,T}
-                @test isapprox(surfaceintegral(f, circle, rule), area(disk); rtol=1e-3)     # Meshes.Circle
-                @test isapprox(surfaceintegral(f, disk, rule), area(disk); rtol=1e-3)       # Meshes.Disk
-                #@test isapprox(surfaceintegral(f, sphere2d, rule), area(disk); rtol=1e-3)   # Meshes.Sphere{2,T}
+                @test isapprox(surfaceintegral(f, triangle, rule), 1.0; rtol=1e-3)      # Meshes.Triangle
+                @test isapprox(surfaceintegral(f, box2d, rule), 4.0; rtol=1e-3)         # Meshes.Box{2,T}
+                @test isapprox(surfaceintegral(f, disk, rule), area(disk); rtol=1e-3)   # Meshes.Disk
             end
             @testset "Vector-Valued Functions" begin
                 f(::Point) = [1.0, 1.0, 1.0]
 
                 # Line Integrals
                 @test lineintegral(f, seg_ne, rule) ≈ [sqrt(2), sqrt(2), sqrt(2)]              # Meshes.Segment
-                @test lineintegral(f, rect_traj_ring, rule) ≈ [4sqrt(2), 4sqrt(2), 4sqrt(2)]   # Meshes.Ring
-                @test lineintegral(f, rect_traj_rope, rule) ≈ [4sqrt(2), 4sqrt(2), 4sqrt(2)]   # Meshes.Rope
-                @test lineintegral(f, unit_bezier, rule) ≈ length(unit_bezier) .* [1.0, 1.0, 1.0]    # Meshes.BezierCurve
+                @test lineintegral(f, ring_rect, rule) ≈ [4sqrt(2), 4sqrt(2), 4sqrt(2)]   # Meshes.Ring
+                @test lineintegral(f, rope_rect, rule) ≈ [4sqrt(2), 4sqrt(2), 4sqrt(2)]   # Meshes.Rope
+                @test lineintegral(f, bezier, rule) ≈ length(bezier) .* [1.0, 1.0, 1.0]    # Meshes.BezierCurve
                 @test lineintegral(f, circle, rule) ≈ length(circle) .* [1.0, 1.0, 1.0]        # Meshes.Circle
-                @test lineintegral(f, disk, rule) ≈ length(circle) .* [1.0, 1.0, 1.0]          # Meshes.Disk
                 @test lineintegral(f, sphere2d, rule) ≈ length(sphere2d) .* [1.0, 1.0, 1.0]    # Meshes.Sphere{2,T}
-                @test lineintegral(f, triangle, rule) ≈ (2 + 2sqrt(2)) .* [1.0, 1.0, 1.0]      # Meshes.Triangle
-                @test lineintegral(f, box2d, rule) ≈ [8.0, 8.0, 8.0]                           # Meshes.Box{2,T}
 
                 # Surface Integrals
                 @test isapprox(surfaceintegral(f, triangle, rule), [1.0, 1.0, 1.0]; rtol=1e-3)   # Meshes.Triangle
                 @test isapprox(surfaceintegral(f, box2d, rule), [4.0, 4.0, 4.0]; rtol=1e-3)      # Meshes.Box{2,T}
-                @test isapprox(surfaceintegral(f, circle, rule), area(disk) .* [1.0, 1.0, 1.0]; rtol=1e-3)   # Meshes.Circle
                 @test isapprox(surfaceintegral(f, disk, rule), area(disk) .* [1.0, 1.0, 1.0]; rtol=1e-3)     # Meshes.Disk
-                #@test isapprox(surfaceintegral(f, sphere2d, rule), area(disk) .* [1.0, 1.0, 1.0]; rtol=1e-3) # Meshes.Sphere{2,T}
             end
         end
     end
@@ -128,8 +147,8 @@ end
 
     # Rectangular trajectory CCW around the four points
     rect_traj_segs = [seg_ne, seg_nw, seg_sw, seg_se]
-    rect_traj_ring = Ring(pt_e, pt_n, pt_w, pt_s)
-    rect_traj_rope = Rope(pt_e, pt_n, pt_w, pt_s, pt_e)
+    ring_rect = Ring(pt_e, pt_n, pt_w, pt_s)
+    rope_rect = Rope(pt_e, pt_n, pt_w, pt_s, pt_e)
 
     # Approximately circular trajectory CCW around the unit-meter circle
     unit_circle = BezierCurve(
@@ -140,8 +159,8 @@ end
         f(::Point{Dim,T}) where {Dim,T} = 1.0Ω/m
         @test lineintegral(f, seg_ne) ≈ sqrt(2)*Ω                         # Meshes.Segment
         @test lineintegral(f, rect_traj_segs) ≈ 4sqrt(2)*Ω                # Vector{::Meshes.Segment}
-        @test lineintegral(f, rect_traj_ring) ≈ 4sqrt(2)*Ω                # Meshes.Ring
-        @test lineintegral(f, rect_traj_rope) ≈ 4sqrt(2)*Ω                # Meshes.Rope
+        @test lineintegral(f, ring_rect) ≈ 4sqrt(2)*Ω                # Meshes.Ring
+        @test lineintegral(f, rope_rect) ≈ 4sqrt(2)*Ω                # Meshes.Rope
         @test isapprox(lineintegral(f, unit_circle), 2π*Ω; atol=0.15Ω)    # Meshes.BezierCurve
     end
 
@@ -149,8 +168,8 @@ end
         f(::Point{Dim,T}) where {Dim,T} = [1.0Ω/m, 1.0Ω/m, 1.0Ω/m]
         @test lineintegral(f, seg_ne) ≈ [sqrt(2), sqrt(2), sqrt(2)] .* Ω                  # Meshes.Segment
         @test lineintegral(f, rect_traj_segs)  ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω    # Vector{::Meshes.Segment}
-        @test lineintegral(f, rect_traj_ring) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Ring
-        @test lineintegral(f, rect_traj_rope) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Rope
+        @test lineintegral(f, ring_rect) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Ring
+        @test lineintegral(f, rope_rect) ≈ 4 .* [sqrt(2), sqrt(2), sqrt(2)] .* Ω     # Meshes.Rope
         @test isapprox(lineintegral(f, unit_circle), [2π, 2π, 2π] .* Ω; atol=0.15Ω)    # Meshes.BezierCurve
     end
 end
@@ -182,8 +201,8 @@ end
 
     # Rectangular trajectory CCW around the four points
     rect_traj_segs = [seg_ne, seg_nw, seg_sw, seg_se]
-    rect_traj_ring = Ring(pt_e, pt_n, pt_w, pt_s)
-    rect_traj_rope = Rope(pt_e, pt_n, pt_w, pt_s, pt_e)
+    ring_rect = Ring(pt_e, pt_n, pt_w, pt_s)
+    rope_rect = Rope(pt_e, pt_n, pt_w, pt_s, pt_e)
 
     # Approximately circular trajectory CCW around the unit-meter circle
     unit_circle = BezierCurve(
@@ -194,8 +213,8 @@ end
         f(::Point{Dim,T}) where {Dim,T} = 1.0Ω/m
         @test lineintegral(f, seg_ne) ≈ sqrt(2)*Ω                         # Meshes.Segment
         @test lineintegral(f, rect_traj_segs) ≈ 4sqrt(2)*Ω                # Vector{::Meshes.Segment}
-        @test lineintegral(f, rect_traj_ring) ≈ 4sqrt(2)*Ω                # Meshes.Ring
-        @test lineintegral(f, rect_traj_rope) ≈ 4sqrt(2)*Ω                # Meshes.Rope
+        @test lineintegral(f, ring_rect) ≈ 4sqrt(2)*Ω                # Meshes.Ring
+        @test lineintegral(f, rope_rect) ≈ 4sqrt(2)*Ω                # Meshes.Rope
         @test isapprox(lineintegral(f, unit_circle), 2π*Ω; atol=0.15)    # Meshes.BezierCurve
               # TODO change 0.15 => 0.15Ω once DynamicQuantities PR approved
     end
@@ -204,8 +223,8 @@ end
         f(::Point{Dim,T}) where {Dim,T} = [1.0Ω/m, 1.0Ω/m, 1.0Ω/m]
         @test all(isapprox.(lineintegral(f, seg_ne), sqrt(2)*Ω))                # Meshes.Segment
         @test all(isapprox.(lineintegral(f, rect_traj_segs), 4sqrt(2)*Ω))       # Vector{::Meshes.Segment}
-        @test all(isapprox.(lineintegral(f, rect_traj_ring), 4sqrt(2)*Ω))       # Meshes.Ring
-        @test all(isapprox.(lineintegral(f, rect_traj_rope), 4sqrt(2)*Ω))       # Meshes.Rope
+        @test all(isapprox.(lineintegral(f, ring_rect), 4sqrt(2)*Ω))       # Meshes.Ring
+        @test all(isapprox.(lineintegral(f, rope_rect), 4sqrt(2)*Ω))       # Meshes.Rope
         @test all(isapprox.(lineintegral(f, unit_circle), 2π*Ω; atol=0.15))    # Meshes.BezierCurve
              # TODO change 0.15 => 0.15Ω once DynamicQuantities PR approved
     end
