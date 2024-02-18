30e7e450a4f76e4d01a76ce890aedbd7b1609422
Michael Ingold <mike.ingold@gmail.com>
2024-02-18 15:48:23 -0500

    Improved surfaceintegral on Triangle with GaussLegendre

--- src/integral_surface.jl
+++ src/integral_surface.jl
@@ -85,6 +85,7 @@ end
     surfaceintegral(f, triangle::Meshes.Triangle, ::GaussLegendre)
 
 Like [`surfaceintegral`](@ref) but integrates over the surface of a `triangle`
+by transforming the triangle into a polar-barycentric coordinate system and
 using a Gauss-Legendre quadrature rule along each barycentric dimension of the
 triangle.
 """
@@ -101,29 +102,29 @@ function surfaceintegral(
     wws = Iterators.product(ws, ws)
     xxs = Iterators.product(xs, xs)
 
-    # Domain transformation: u,v [-1,1] ↦ s,t [0,1]
-    s(u) = 0.5u + 0.5
-    t(v) = 0.5v + 0.5
-    point(xi,xj) = triangle(s(xi), t(xj))
+    # Domain transformations:
+    #   xᵢ [-1,1] ↦ R [0,1]
+    #   xⱼ [-1,1] ↦ φ [0,π/2]
+    uR(xᵢ) = 0.5 * (xᵢ + 1)
+    uφ(xⱼ) = (π/4) * (xⱼ + 1)
 
-    # Determine output type of f at a Point inside the triangle
-    # Define an applicable zero value
-    fzero = zero(f(point(-0.5,-0.5)))
-
-    # Calculate weight-node product
-    function g(((wi,wj), (xi,xj)))
-        if 0 < (s(xi) + t(xj)) < 1
-            # Valid coordinate (inside triangle)
-            return wi * wj * f(point(xi,xj))
-        else
-            # Invalid coordinate (outside triangle)
-            return fzero
-        end
+    # Integrate the Barycentric triangle by transforming it into polar coordinates
+    #   with a modified radius
+    #     R = r ( sinφ + cosφ )
+    #   s.t. integration bounds become rectangular
+    #     R ∈ [0, 1] and φ ∈ [0, π/2]
+    function integrand(((wᵢ,wⱼ), (xᵢ,xⱼ)))
+        R = uR(xᵢ)
+        φ = uφ(xⱼ)
+        a,b = sincos(φ)
+        u = R * (1 - a/(a+b))
+        v = R * (1 - b/(a+b))
+        return wᵢ * wⱼ * f(triangle(u,v)) * R / (a+b)^2
     end
 
-    # Calculate 2D Gauss-Legendre integral of f over Barycentric coordinates [-1,1]^2
-    # Apply a linear domain-correction factor [-1,1]^2 ↦ area(triangle)
-    return 0.5 * area(triangle) .* sum(g, zip(wws,xxs))
+    # Calculate 2D Gauss-Legendre integral over modified-polar-Barycentric coordinates
+    # Apply a linear domain-correction factor
+    return (π/4) * area(triangle) .* sum(integrand, zip(wws,xxs))
 end
 
 ################################################################################
