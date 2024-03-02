fe24734427819eeda7878b087c82a21e8634b663
Michael Ingold <mike.ingold@gmail.com>
2024-03-01 20:44:28 -0500

    Add generalized 3D GaussLegendre

--- src/integral_volume.jl
+++ src/integral_volume.jl
@@ -2,6 +2,32 @@
 #                               Gauss-Legendre
 ################################################################################
 
+function _integral_3d(f, geometry3d, settings::GaussLegendre)
+    # Get Gauss-Legendre nodes and weights for a 2D region [-1,1]^2
+    xs, ws = gausslegendre(settings.n)
+    wws = Iterators.product(ws, ws, ws)
+    xxs = Iterators.product(xs, xs, xs)
+
+    # Domain transformation: x [-1,1] ↦ s,t,u [0,1]
+    s(x) = 0.5x + 0.5
+    t(x) = 0.5x + 0.5
+    u(x) = 0.5x + 0.5
+
+    point(stu) = geometry3d(stu[1], stu[2], stu[3])
+
+    function paramfactor(stu)
+        J = jacobian(geometry3d, stu)
+        return abs((J[1] × J[2]) ⋅ J[3])
+    end
+
+    function integrand(((wi,wj,wk), (xi,xj,xk)))
+        stu = [s(xi),t(xj),u(xk)]
+        wi * wj * wk * f(point(stu)) * paramfactor(stu)
+    end
+
+    return (1/8) .* sum(g, zip(wws,xxs))
+end
+
 function integral(
     f,
     ball::Meshes.Ball{3,T},
@@ -10,21 +36,7 @@ function integral(
     # Validate the provided integrand function
     _validate_integrand(f,3,T)
 
-    # Get Gauss-Legendre nodes and weights for a 2D region [-1,1]^2
-    xs, ws = gausslegendre(settings.n)
-    wws = Iterators.product(ws, ws, ws)
-    xxs = Iterators.product(xs, xs, xs)
-
-    # Domain transformation: xi,xj,xk [-1,1] ↦ s,t,u [0,1]
-    s(xi) = 0.5xi + 0.5
-    t(xj) = 0.5xj + 0.5
-    u(xk) = 0.5xk + 0.5
-
-    # Integrate the ball in parametric (s,t,u)-space [0,1]³
-    integrand(s,t,u) = s^2 * sinpi(t) * f(ball(s,t,u))
-    g(((wi,wj,wk), (xi,xj,xk))) = wi * wj * wk * integrand(s(xi),t(xj),u(xk))
-    R = ball.radius
-    return (1/8) * 2π^2 * R^3 .* sum(g, zip(wws,xxs))
+    return _integral_3d(f, ball, settings)
 end
 
 function integral(
@@ -35,23 +47,7 @@ function integral(
     # Validate the provided integrand function
     _validate_integrand(f,3,T)
 
-    # Get Gauss-Legendre nodes and weights for a 3D region [-1,1]³
-    xs, ws = gausslegendre(settings.n)
-    wws = Iterators.product(ws, ws, ws)
-    xxs = Iterators.product(xs, xs, xs)
-
-    # Domain transformation: xi,xj,xk [-1,1] ↦ s,t,u [0,1]
-    s(xi) = 0.5xi + 0.5
-    t(xj) = 0.5xj + 0.5
-    u(xk) = 0.5xk + 0.5
-    point(xi,xj,xk) = box(s(xi), t(xj), u(xk))
-
-    # Calculate weight-node product
-    g(((wi,wj,wk), (xi,xj,xk))) = wi * wj * wk * f(point(xi,xj,xk))
-
-    # Calculate 3D Gauss-Legendre integral of f over parametric coordinates [-1,1]³
-    # Apply a linear domain-correction factor [-1,1]³ ↦ volume(box)
-    return (1/8) * volume(box) .* sum(g, zip(wws,xxs))
+    return _integral_3d(f, box, settings)
 end
 
 
