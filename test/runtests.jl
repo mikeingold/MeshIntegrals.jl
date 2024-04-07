d15932cb46dfaa16251239aedcb6d69b3572aefb
Michael Ingold <mike.ingold@gmail.com>
2024-04-07 17:33:19 -0400

    Disable Aqua ambiguities checks

--- test/runtests.jl
+++ test/runtests.jl
@@ -188,4 +188,6 @@ end
 #                                Aqua.jl Tests
 ################################################################################
 
-Aqua.test_all(MeshIntegrals)
+# As of v0.11.4 -- Ambiguities check disabled since it fails due to upstream findings
+#   Verified that no ambiguities exist within MeshIntegrals.jl
+Aqua.test_all(MeshIntegrals; ambiguities = false)
