bc37197ec0ffaa78e6a417bf175602d58f9b4895
Michael Ingold <mike.ingold@gmail.com>
2024-09-23 19:21:34 -0400

    Change Algorithm to Rule and update docstrings

--- src/integration_rules.jl
+++ src/integration_rules.jl
@@ -1,16 +1,18 @@
 ################################################################################
-#                         Integration Algorithms
+#                           Integration Rules
 ################################################################################
 
-abstract type IntegrationAlgorithm end
+abstract type IntegrationRule end
 
 """
     GaussKronrod(kwargs...)
 
-Numerically integrate using the h-adaptive Gauss-Kronrod quadrature rule implemented
-by QuadGK.jl. All standard `QuadGK.quadgk` keyword arguments are supported.
+The h-adaptive Gauss-Kronrod quadrature rule implemented by QuadGK.jl. All standard
+`QuadGK.quadgk` keyword arguments are supported. This rule works natively for one
+dimensional geometries; some two- and three-dimensional geometries are additionally
+supported using nested integral solvers with the specified `kwarg` settings.
 """
-struct GaussKronrod <: IntegrationAlgorithm
+struct GaussKronrod <: IntegrationRule
     kwargs
     GaussKronrod(; kwargs...) = new(kwargs)
 end
@@ -18,27 +20,26 @@ end
 """
     GaussLegendre(n)
 
-Numerically integrate using an `n`'th-order Gauss-Legendre quadrature rule. Nodes
-and weights are efficiently calculated using FastGaussQuadrature.jl.
+An `n`'th-order Gauss-Legendre quadrature rule. Nodes and weights are
+efficiently calculated using FastGaussQuadrature.jl.
 
 So long as the integrand function can be well-approximated by a polynomial of
 order `2n-1`, this method should yield results with 16-digit accuracy in `O(n)`
 time. If the function is know to have some periodic content, then `n` should
 (at a minimum) be greater than the expected number of periods over the geometry,
-e.g. `length(geometry)/lambda`.
+e.g. `length(geometry)/λ`.
 """
-struct GaussLegendre <: IntegrationAlgorithm
+struct GaussLegendre <: IntegrationRule
     n::Int64
 end
 
 """
     HAdaptiveCubature(kwargs...)
 
-Numerically integrate areas and surfaces using the h-adaptive cubature rule
-implemented by HCubature.jl. All standard `HCubature.hcubature` keyword
-arguments are supported.
+The h-adaptive cubature rule implemented by HCubature.jl. All standard
+`HCubature.hcubature` keyword arguments are supported.
 """
-struct HAdaptiveCubature <: IntegrationAlgorithm
+struct HAdaptiveCubature <: IntegrationRule
     kwargs
     HAdaptiveCubature(; kwargs...) = new(kwargs)
 end
