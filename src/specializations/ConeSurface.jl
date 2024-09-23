7a256da10e0e3658770b693f05db23c2738d29e6
Michael Ingold <mike.ingold@gmail.com>
2024-09-22 20:44:46 -0400

    Consolidate conic methods under new structure

--- src/specializations/ConeSurface.jl
+++ src/specializations/ConeSurface.jl
@@ -9,21 +9,6 @@ function integral(
     FP::Type{T} = Float64
 ) where {F<:Function, I<:IntegrationAlgorithm, T<:AbstractFloat}
     # The generic method only parameterizes the sides
-    sides = _integral_2d(f, cone, settings, FP)
-
-    # Integrate the Disk at the base
-    base = _integral_2d(f, cone.base, settings, FP)
-
-    return sides + base
-end
-
-function integral(
-    f::F,
-    cone::Meshes.ConeSurface,
-    settings::HAdaptiveCubature,
-    FP::Type{T} = Float64
-) where {F<:Function, T<:AbstractFloat}
-    # The generic method only parameterizes the sides
     sides = _integral(f, cone, settings, FP)
 
     # Integrate the Disk at the base
--- src/specializations/FrustumSurface.jl
+++ src/specializations/FrustumSurface.jl
@@ -7,23 +7,7 @@ function integral(
     frust::Meshes.FrustumSurface,
     settings::I,
     FP::Type{T} = Float64
-) where {F<:Function, I<:IntegrationAlgorithm, T<:AbstractFloat}
-    # The generic method only parameterizes the sides
-    sides = _integral_2d(f, frust, settings, FP)
-
-    # Integrate the Disks at the top and bottom
-    top = _integral_2d(f, frust.top, settings, FP)
-    bottom = _integral_2d(f, frust.bot, settings, FP)
-
-    return sides + top + bottom
-end
-
-function integral(
-    f::F,
-    frust::Meshes.FrustumSurface,
-    settings::HAdaptiveCubature,
-    FP::Type{T} = Float64
-) where {F<:Function, T<:AbstractFloat}
+) where  {F<:Function, I<:IntegrationAlgorithm, T<:AbstractFloat}
     # The generic method only parameterizes the sides
     sides = _integral(f, frust, settings, FP)
 
