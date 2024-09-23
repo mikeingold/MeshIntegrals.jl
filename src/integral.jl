f8c92a3ea60bd34062fe5fd135a527b6c26d8368
Michael Ingold <mike.ingold@gmail.com>
2024-09-23 19:28:54 -0400

    Change algorithm to rule

--- src/integral.jl
+++ src/integral.jl
@@ -4,17 +4,17 @@
 
 """
     integral(f, geometry)
-    integral(f, geometry, algorithm)
-    integral(f, geometry, algorithm, FP)
+    integral(f, geometry, rule)
+    integral(f, geometry, rule, FP)
 
 Numerically integrate a given function `f(::Point)` over the domain defined by
-a `geometry` using a particular `integration algorithm` with floating point
+a `geometry` using a particular numerical `integration rule` with floating point
 precision of type `FP`.
 
 # Arguments
 - `f`: an integrand function with a method `f(::Meshes.Point)`
 - `geometry`: some `Meshes.Geometry` that defines the integration domain
-- `algorithm`: optionally, the `IntegrationAlgorithm` used for the integration (by default `GaussKronrod()` in 1D and `HAdaptiveCubature()` else)
+- `rule`: optionally, the `IntegrationRule` used for integration (by default `GaussKronrod()` in 1D and `HAdaptiveCubature()` else)
 - `FP`: optionally, the floating point precision desired (`Float64` by default)
 
 Note that reducing `FP` below `Float64` will incur some loss of precision. By
@@ -23,7 +23,7 @@ contrast, increasing `FP` to e.g. `BigFloat` will typically increase precision
 """
 function integral end
 
-# If only f and geometry are specified, select default algorithm
+# If only f and geometry are specified, select default rule
 function integral(
     f::F,
     geometry::G
@@ -33,14 +33,14 @@ function integral(
     _integral(f, geometry, rule)
 end
 
-# with algorithm and T specified
+# with rule and T specified
 function integral(
     f::F,
     geometry::G,
-    settings::I,
+    rule::I,
     FP::Type{T} = Float64
-) where {F<:Function, G<:Meshes.Geometry, I<:IntegrationAlgorithm, T<:AbstractFloat}
-    _integral(f, geometry, settings, FP)
+) where {F<:Function, G<:Meshes.Geometry, I<:IntegrationRule, T<:AbstractFloat}
+    _integral(f, geometry, rule, FP)
 end
 
 
@@ -52,17 +52,17 @@ end
 function _integral(
     f,
     geometry,
-    settings::GaussKronrod,
+    rule::GaussKronrod,
     FP::Type{T} = Float64
 ) where {T<:AbstractFloat}
     # Run the appropriate integral type
     Dim = Meshes.paramdim(geometry)
     if Dim == 1
-        return _integral_1d(f, geometry, settings, FP)
+        return _integral_1d(f, geometry, rule, FP)
     elseif Dim == 2
-        return _integral_2d(f, geometry, settings, FP)
+        return _integral_2d(f, geometry, rule, FP)
     elseif Dim == 3
-        return _integral_3d(f, geometry, settings, FP)
+        return _integral_3d(f, geometry, rule, FP)
     end
 end
 
@@ -70,13 +70,13 @@ end
 function _integral(
     f,
     geometry,
-    settings::GaussLegendre,
+    rule::GaussLegendre,
     FP::Type{T} = Float64
 ) where {T<:AbstractFloat}
     N = Meshes.paramdim(geometry)
 
     # Get Gauss-Legendre nodes and weights for a region [-1,1]^N
-    xs, ws = _gausslegendre(FP, settings.n)
+    xs, ws = _gausslegendre(FP, rule.n)
     weights = Iterators.product(ntuple(Returns(ws), N)...)
     nodes = Iterators.product(ntuple(Returns(xs), N)...)
 
@@ -95,7 +95,7 @@ end
 function _integral(
     f,
     geometry,
-    settings::HAdaptiveCubature,
+    rule::HAdaptiveCubature,
     FP::Type{T} = Float64
 ) where {T<:AbstractFloat}
     Dim = Meshes.paramdim(geometry)
@@ -109,7 +109,7 @@ function _integral(
     # Create a wrapper that returns only the value component in those units
     uintegrand(uv) = Unitful.ustrip.(integrandunits, integrand(uv))
     # Integrate only the unitless values
-    value = HCubature.hcubature(uintegrand, zeros(FP,Dim), ones(FP,Dim); settings.kwargs...)[1]
+    value = HCubature.hcubature(uintegrand, zeros(FP,Dim), ones(FP,Dim); rule.kwargs...)[1]
 
     # Reapply units
     return value .* integrandunits
@@ -122,7 +122,7 @@ end
 function _integral_1d(
     f,
     geometry,
-    settings::GaussKronrod,
+    rule::GaussKronrod,
     FP::Type{T} = Float64
 ) where {T<:AbstractFloat}
     integrand(t) = f(geometry(t)) * differential(geometry, [t])
@@ -132,7 +132,7 @@ end
 function _integral_2d(
     f,
     geometry2d,
-    settings::GaussKronrod,
+    rule::GaussKronrod,
     FP::Type{T} = Float64
 ) where {T<:AbstractFloat}
     integrand(u,v) = f(geometry2d(u,v)) * differential(geometry2d, [u,v])
@@ -144,7 +144,7 @@ end
 function _integral_3d(
     f,
     geometry,
-    settings::GaussKronrod,
+    rule::GaussKronrod,
     FP::Type{T} = Float64
 ) where {T<:AbstractFloat}
     error("Integrating this volume type with GaussKronrod not supported.")
