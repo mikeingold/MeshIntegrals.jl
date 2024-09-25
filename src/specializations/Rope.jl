3c7d33b9e834e9d2ab8b65c87a67a7f613774bd5
Michael Ingold <mike.ingold@gmail.com>
2024-09-25 13:20:39 -0400

    Add why-special notes for Ring and Rope

--- src/specializations/Rope.jl
+++ src/specializations/Rope.jl
@@ -1,5 +1,11 @@
 ################################################################################
 #                      Specialized Methods for Rope
+#
+# Why Specialized?
+#   The Rope geometry defines a polytope whose length spans segments between
+#   consecutive points. Meshes.jl does not define a parametric function for
+#   Rope's, but they can be decomposed into their constituent Segment's,
+#   integrated separately, and then summed.
 ################################################################################
 
 function integral(
