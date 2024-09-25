d784136f3908141039436751b126b3f9740d1432
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2024-09-26 01:30:59 +0200

    Add testing with ExplicitImports.jl (#90)
    * add tests with ExplicitImports.jl
    
    * add newline at end of file
    
    * add some more tests
    
    * bump lower compat of ExplicitImport.jl
    
    * import × from Meshes.jl
    
    * import ⋅ from Meshes.jl
    
    * Move cross and dot imports to LA
    
    ---------
    
    Co-authored-by: Michael Ingold <mike.ingold@gmail.com>

--- test/aqua.jl
+++ test/aqua.jl
@@ -5,3 +5,17 @@
     # - Verified that no ambiguities exist within MeshIntegrals.jl
     Aqua.test_all(MeshIntegrals; ambiguities = false)
 end
+
+@testitem "ExplicitImports.jl" begin
+    using ExplicitImports: check_no_implicit_imports, check_no_stale_explicit_imports,
+                           check_all_explicit_imports_via_owners,
+                           check_all_explicit_imports_are_public,
+                           check_all_qualified_accesses_via_owners,
+                           check_no_self_qualified_accesses
+    @test isnothing(check_no_implicit_imports(MeshIntegrals))
+    @test isnothing(check_no_stale_explicit_imports(MeshIntegrals))
+    @test isnothing(check_all_explicit_imports_via_owners(MeshIntegrals))
+    @test isnothing(check_all_explicit_imports_are_public(MeshIntegrals))
+    @test isnothing(check_all_qualified_accesses_via_owners(MeshIntegrals))
+    @test isnothing(check_no_self_qualified_accesses(MeshIntegrals))
+end
