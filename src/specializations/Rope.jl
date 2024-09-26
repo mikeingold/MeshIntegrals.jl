020a4a29c7bae3b81f9af32b57c4633851792bfe
Michael Ingold <mike.ingold@gmail.com>
2024-09-25 22:21:01 -0400

    Convert FP to kwarg

--- src/specializations/Rope.jl
+++ src/specializations/Rope.jl
@@ -11,37 +11,9 @@
 function integral(
         f::F,
         rope::Meshes.Rope,
-        rule::HAdaptiveCubature
-) where {F <: Function}
-    # Convert the Rope into Segments, sum the integrals of those 
-    return sum(segment -> _integral(f, segment, rule), segments(rope))
-end
-
-function integral(
-        f::F,
-        rope::Meshes.Rope,
-        rule::HAdaptiveCubature,
-        FP::Type{T}
-) where {F <: Function, T <: AbstractFloat}
-    # Convert the Rope into Segments, sum the integrals of those 
-    return sum(segment -> _integral(f, segment, rule, FP), segments(rope))
-end
-
-function integral(
-        f::F,
-        rope::Meshes.Rope,
-        rule::I
-) where {F <: Function, I <: IntegrationRule}
-    # Convert the Rope into Segments, sum the integrals of those 
-    return sum(segment -> integral(f, segment, rule), segments(rope))
-end
-
-function integral(
-        f::F,
-        rope::Meshes.Rope,
-        rule::I,
-        FP::Type{T}
+        rule::I;
+        kwargs...
 ) where {F <: Function, I <: IntegrationRule, T <: AbstractFloat}
     # Convert the Rope into Segments, sum the integrals of those 
-    return sum(segment -> integral(f, segment, rule, FP), segments(rope))
+    return sum(segment -> integral(f, segment, rule; kwargs...), segments(rope))
 end
