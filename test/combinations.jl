38b7959a8bdaeb195e5c281e346e3f45b5537ae7
Michael Ingold <mike.ingold@gmail.com>
2024-10-06 02:28:23 -0400

    Add explicit rtol for 4D Box

--- test/combinations.jl
+++ test/combinations.jl
@@ -181,15 +181,15 @@ end
 
     # Scalar integrand
     sol = 4a^3 * (π * a^2 / 4) * u"Ω"
-    @test integral(f, box, GaussLegendre(100)) ≈ sol
+    @test integral(f, box, GaussLegendre(100))≈sol rtol=1e-6
     @test_throws "not supported" integral(f, box, GaussKronrod())
-    @test integral(f, box, HAdaptiveCubature()) ≈ sol
+    @test integral(f, box, HAdaptiveCubature(rtol=1e-6))≈sol rtol=1e-6
 
     # Vector integrand
     vsol = fill(sol, 3)
-    @test integral(fv, box, GaussLegendre(100)) ≈ vsol
+    @test integral(fv, box, GaussLegendre(100))≈vsol rtol=1e-6
     @test_throws "not supported" integral(fv, box, GaussKronrod())
-    @test integral(fv, box, HAdaptiveCubature()) ≈ vsol
+    @test integral(fv, box, HAdaptiveCubature(rtol=1e-6))≈vsol rtol=1e-6
 
     # Integral aliases
     @test_throws "not supported" lineintegral(f, box)
