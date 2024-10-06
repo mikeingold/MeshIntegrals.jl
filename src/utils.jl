be9804e14224aed23e8df5fc4b930bc9353e3e27
Michael Ingold <mike.ingold@gmail.com>
2024-10-05 21:41:30 -0400

    Add _clifford utility

--- src/utils.jl
+++ src/utils.jl
@@ -10,3 +10,10 @@ end
 
 # Extract the length units used by the CRS of a Point
 _units(pt::Meshes.Point{M, CRS}) where {M, CRS} = first(CoordRefSystems.units(CRS))
+
+# Meshes.Vec -> CliffordNumber.KVector
+function _clifford(v::Meshes.Vec{Dim, T}) where {Dim, T}
+    units = Unitful.unit(T)
+    cliffordnumber = KVector{1, VGA(Dim)}(Unitful.ustrip.(units, v.coords...))
+    return (units, cliffordnumber)
+end
