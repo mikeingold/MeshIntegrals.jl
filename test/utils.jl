14934b5840ee374b49c74f13eac7f985e3ab3538
Michael Ingold <mike.ingold@gmail.com>
2024-10-08 22:41:08 -0400

    Remove tests for _ukvector

--- test/utils.jl
+++ test/utils.jl
@@ -5,12 +5,6 @@
     v = Meshes.Vec(3, 4)
     @test norm(MeshIntegrals._kvector(v)) ≈ 5.0
 
-    # _ukvector
-    v = Meshes.Vec(3, 4)
-    (units, kvector) = MeshIntegrals._ukvector(v)
-    @test units == u"m"
-    @test norm(kvector) ≈ 5.0
-
     # _units
     p = Point(1.0u"cm", 2.0u"mm", 3.0u"m")
     @test MeshIntegrals._units(p) == u"m"
