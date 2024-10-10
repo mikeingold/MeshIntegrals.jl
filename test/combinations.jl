a3a9c866af8a1681c549720bc2f14351af37a710
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-10-10 16:43:19 +0200

    fix typo in version number

--- test/combinations.jl
+++ test/combinations.jl
@@ -517,9 +517,9 @@ end
 end
 
 @testitem "ParametrizedCurve" setup=[Setup] begin
-    # ParametrizedCurve has been added in Meshes v0.5.20
+    # ParametrizedCurve has been added in Meshes v0.51.20
     # If the version is specified as minimal compat bound in the Project.toml, the downgrade test fails
-    if pkgversion(Meshes) >= v"0.5.20"
+    if pkgversion(Meshes) >= v"0.51.20"
         radius = 4.4
         curve_cart = ParametrizedCurve(
             t -> Point(radius * cos(t), radius * sin(t)), (0.0, 2π))
