7ecdfae5bf2c7a38a45546a10082b36d16a58d73
Documenter.jl <documenter@juliadocs.github.io>
2024-10-10 10:58:09 +0000

    delete history

---
+++ v0.13.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MeshIntegrals.jl","page":"MeshIntegrals.jl","title":"MeshIntegrals.jl","text":"","category":"section"},{"location":"","page":"MeshIntegrals.jl","title":"MeshIntegrals.jl","text":"MeshIntegrals.jl is a Julia library that leverages differential forms to implement fast and easy numerical integration of field equations over geometric domains.","category":"page"}]
+}
