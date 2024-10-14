c5e8e33da4833392144679a19b884fa985ccf02b
Michael Ingold <mike.ingold@gmail.com>
2024-10-14 16:54:46 -0400

    Formatting
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using TestItemRunner
 using TestItems
 
 # For CI, run all tests not marked with the :extended tag
-@run_package_tests filter=ti->!(:extended in ti.tags) verbose=true
+@run_package_tests filter=ti -> !(:extended in ti.tags) verbose=true
 
 @testsnippet Setup begin
     using Meshes
