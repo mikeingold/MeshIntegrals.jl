4a6b8a87274a2e8e68224c23947ef98602fd740e
Michael Ingold <mike.ingold@gmail.com>
2024-10-23 18:32:21 -0400

    Update type params and move _units function

--- src/utils.jl
+++ src/utils.jl
@@ -8,11 +8,6 @@ function _gausslegendre(T, n)
     return T.(xs), T.(ws)
 end
 
-# Extract the length units used by the CRS of a Geometry
-function _units(g::Meshes.Geometry{M, CRS}) where {M, CRS}
-    return Unitful.unit(CoordRefSystems.lentype(CRS))
-end
-
 # Common error message structure
 function _error_unsupported_combination(geometry, rule)
     msg = "Integrating a $geometry using a $rule rule not supported."
@@ -20,7 +15,7 @@ function _error_unsupported_combination(geometry, rule)
 end
 
 ################################################################################
-#                        CliffordNumbers Interface
+#                        CliffordNumbers and Units
 ################################################################################
 
 # Meshes.Vec -> ::CliffordNumber.KVector
@@ -28,3 +23,6 @@ function _kvector(v::Meshes.Vec{Dim, T}) where {Dim, T}
     ucoords = Iterators.map(Unitful.ustrip, v.coords)
     return CliffordNumbers.KVector{1, VGA(Dim)}(ucoords...)
 end
+
+# Extract the length units used by the CRS of a Geometry
+_units(::Geometry{M, CRS}) where {M, CRS} = Unitful.unit(CoordRefSystems.lentype(CRS))
