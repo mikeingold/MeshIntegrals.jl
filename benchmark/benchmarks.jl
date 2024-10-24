8249355bcb46370506fe08d6bc37214a211b28ea
Michael Ingold <mike.ingold@gmail.com>
2024-10-23 20:37:49 -0400

    Simplify

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -14,19 +14,16 @@ SUITE["Integrals"]["Meshes.Segment"] = let s = BenchmarkGroup()
     pt_b = Point(sin(θ) * cos(φ) * u"m", sin(θ) * sin(φ) * u"m", cos(θ) * u"m")
     segment = Segment(pt_a, pt_b)
 
-    function f(p::P) where {P <: Meshes.Point}
-        a, b = (7.1, 4.6)  # arbitrary constants > 0
-        r = ustrip(u"m", norm(to(p)))
-        exp(r * log(a) + (1 - r) * log(b))
-    end
+    f(p) = norm(to(p))
     fv(p) = fill(f(p), 3)
 
-    s["Scalar-valued GaussLegendre"] = @benchmarkable integral(f, segment, GaussLegendre(100)) evals=1000
-    s["Scalar-valued GaussKronrod"] = @benchmarkable integral(f, segment, GaussKronrod()) evals=1000
-    s["Scalar-valued HAdaptiveCubature"] = @benchmarkable integral(f, segment, HAdaptiveCubature()) evals=1000
+    s["Scalar-valued GaussLegendre"] = @benchmarkable integral($f, $segment, GaussLegendre(100)) evals=1000
+    s["Scalar-valued GaussKronrod"] = @benchmarkable integral($f, $segment, GaussKronrod()) evals=1000
+    s["Scalar-valued HAdaptiveCubature"] = @benchmarkable integral($f, $segment, HAdaptiveCubature()) evals=1000
 
-    s["Vector-valued GaussLegendre"] = @benchmarkable integral(fv, segment, GaussLegendre(100)) evals=1000
-    s["Vector-valued GaussKronrod"] = @benchmarkable integral(fv, segment, GaussKronrod()) evals=1000
-    s["Vector-valued HAdaptiveCubature"] = @benchmarkable integral(fv, segment, HAdaptiveCubature()) evals=1000
+    s["Vector-valued GaussLegendre"] = @benchmarkable integral($fv, $segment, GaussLegendre(100)) evals=1000
+    s["Vector-valued GaussKronrod"] = @benchmarkable integral($fv, $segment, GaussKronrod()) evals=1000
+    s["Vector-valued HAdaptiveCubature"] = @benchmarkable integral($fv, $segment, HAdaptiveCubature()) evals=1000
+
     s
 end
