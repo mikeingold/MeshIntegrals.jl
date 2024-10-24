0b85443e44742c59b36d1bf2e57235fa1bd3ed95
Michael Ingold <mike.ingold@gmail.com>
2024-10-23 21:38:42 -0400

    Reorganize

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -8,16 +8,15 @@ const SUITE = BenchmarkGroup()
 SUITE["Integrals"] = BenchmarkGroup()
 
 segment = Segment(Point(0, 0, 0), Point(1, 1, 1))
-f(p) = norm(to(p))
+f(p) = LinearAlgebra.norm(to(p))
 fv(p) = fill(f(p), 3)
 
-SUITE["Integrals"]["Meshes.Segment"] = BenchmarkGroup()
-let s = SUITE["Integrals"]["Meshes.Segment"]
+SUITE["Integrals"]["Meshes.Segment"] = let s = BenchmarkGroup()
     s["Scalar GaussLegendre"] = @benchmarkable integral($f, $segment, GaussLegendre(100)) evals=1000
     s["Scalar GaussKronrod"] = @benchmarkable integral($f, $segment, GaussKronrod()) evals=1000
     s["Scalar HAdaptiveCubature"] = @benchmarkable integral($f, $segment, HAdaptiveCubature()) evals=1000
-
     s["Vector GaussLegendre"] = @benchmarkable integral($fv, $segment, GaussLegendre(100)) evals=1000
     s["Vector GaussKronrod"] = @benchmarkable integral($fv, $segment, GaussKronrod()) evals=1000
     s["Vector HAdaptiveCubature"] = @benchmarkable integral($fv, $segment, HAdaptiveCubature()) evals=1000
+    s
 end
