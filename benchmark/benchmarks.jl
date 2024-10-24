948a12b55f1e846506fda9d231939aa239e9aca5
Michael Ingold <mike.ingold@gmail.com>
2024-10-24 19:32:06 -0400

    Combine multiple geometries into same loop

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,26 +5,26 @@ using MeshIntegrals
 using Unitful
 
 const SUITE = BenchmarkGroup()
-SUITE["Integrals"] = BenchmarkGroup()
-
-segment = Segment(Point(0, 0, 0), Point(1, 1, 1))
-f(p) = norm(to(p))
-fv(p) = fill(f(p), 3)
 
 integrands = (
-    (name = "Scalar", f = f),
-    (name = "Vector", f = fv)
+    (name = "Scalar", f = p -> norm(to(p))),
+    (name = "Vector", f = p -> fill(norm(to(p)), 3))
 )
 rules = (
     (name = "GaussLegendre", rule = GaussLegendre(100)),
     (name = "GaussKronrod", rule = GaussKronrod()),
     (name = "HAdaptiveCubature", rule = HAdaptiveCubature())
 )
+geometries = (
+    (name = "Meshes.Segment", item = Segment(Point(0, 0, 0), Point(1, 1, 1))),
+    (name = "Meshes.Sphere", item = Sphere(Point(0, 0, 0), 1.0))
+)
 
-SUITE["Integrals"]["Meshes.Segment"] = let s = BenchmarkGroup()
-    for (int, rule) in Iterators.product(integrands, rules)
-        name = "$(int.name) $(rule.name)"
-        s[name] = @benchmarkable integral($int.f, $segment, $rule.rule) evals=1000
+SUITE["Integrals"] = let s = BenchmarkGroup()
+    for (int, rule, g) in Iterators.product(integrands, rules, geometries)
+        n1 = geometry.name
+        n2 = "$(int.name) $(rule.name)"
+        s[n1, n2] = @benchmarkable integral($int.f, $geometry.item, $rule.rule) evals=1000
     end
     s
 end
