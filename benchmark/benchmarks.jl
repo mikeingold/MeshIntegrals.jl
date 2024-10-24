c2c1d31a0bccd142c774db31cbee1e466abc7334
Michael Ingold <mike.ingold@gmail.com>
2024-10-24 19:47:18 -0400

    Add N field for evals settings

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -11,9 +11,9 @@ integrands = (
     (name = "Vector", f = p -> fill(norm(to(p)), 3))
 )
 rules = (
-    (name = "GaussLegendre", rule = GaussLegendre(100)),
-    (name = "GaussKronrod", rule = GaussKronrod()),
-    (name = "HAdaptiveCubature", rule = HAdaptiveCubature())
+    (name = "GaussLegendre", rule = GaussLegendre(100), N = 100),
+    (name = "GaussKronrod", rule = GaussKronrod(), N = 100),
+    (name = "HAdaptiveCubature", rule = HAdaptiveCubature(), N = 500)
 )
 geometries = (
     (name = "Meshes.Segment", item = Segment(Point(0, 0, 0), Point(1, 1, 1))),
@@ -22,9 +22,9 @@ geometries = (
 
 SUITE["Integrals"] = let s = BenchmarkGroup()
     for (int, rule, geometry) in Iterators.product(integrands, rules, geometries)
-        n1 = geometry.name
-        n2 = "$(int.name) $(rule.name)"
-        s[n1, n2] = @benchmarkable integral($int.f, $geometry.item, $rule.rule) evals=1000
+        n1, n2 = geometry.name,"$(int.name) $(rule.name)"
+        N = rule.N
+        s[n1, n2] = @benchmarkable integral($int.f, $geometry.item, $rule.rule) evals=N
     end
     s
 end
