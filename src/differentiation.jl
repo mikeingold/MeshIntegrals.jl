34fbca2e092b0faa854354f49a68e1ab4387095c
Michael Ingold <mike.ingold@gmail.com>
2024-10-25 22:45:51 -0400

    Remove obsolete analytic jacobian for BezierCurve

--- src/differentiation.jl
+++ src/differentiation.jl
@@ -72,36 +72,6 @@ function jacobian(
     return ntuple(n -> ∂ₙr(ts, n), Dim)
 end
 
-function jacobian(
-        bz::Meshes.BezierCurve,
-        ts::V
-) where {V <: Union{AbstractVector, Tuple}}
-    t = only(ts)
-    # Parameter t restricted to domain [0,1] by definition
-    if t < 0 || t > 1
-        throw(DomainError(t, "b(t) is not defined for t outside [0, 1]."))
-    end
-
-    # Aliases
-    P = bz.controls
-    N = Meshes.degree(bz)
-
-    # Ensure that this implementation is tractible: limited by ability to calculate
-    #   binomial(N, N/2) without overflow. It's possible to extend this range by
-    #   converting N to a BigInt, but this results in always returning BigFloat's.
-    N <= 1028 || error("This algorithm overflows for curves with ⪆1000 control points.")
-
-    # Generator for Bernstein polynomial functions
-    B(i, n) = t -> binomial(Int128(n), i) * t^i * (1 - t)^(n - i)
-
-    # Derivative = N Σ_{i=0}^{N-1} sigma(i)
-    #   P indices adjusted for Julia 1-based array indexing
-    sigma(i) = B(i, N - 1)(t) .* (P[(i + 1) + 1] - P[(i) + 1])
-    derivative = N .* sum(sigma, 0:(N - 1))
-
-    return (derivative,)
-end
-
 ################################################################################
 #                          Differential Elements
 ################################################################################
