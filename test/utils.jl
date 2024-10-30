695d42526a4d65f24d621198ebe9f9222292d47b
Michael Ingold <mike.ingold@gmail.com>
2024-10-29 23:31:41 -0400

    Typo

--- test/utils.jl
+++ test/utils.jl
@@ -18,8 +18,8 @@
         @test MeshIntegrals.has_analytical(Meshes.Sphere) == false
         @test MeshIntegrals.has_analytical(sphere) == false
 
-        @test MeshIntegrals.default_method(Meshes.BezierCurve) isa Analytic
-        @test MeshIntegrals.default_method(bezier) isa Analytic
+        @test MeshIntegrals.default_method(Meshes.BezierCurve) isa Analytical
+        @test MeshIntegrals.default_method(bezier) isa Analytical
         @test MeshIntegrals.default_method(Meshes.Sphere) isa FiniteDifference
         @test MeshIntegrals.default_method(sphere) isa FiniteDifference
 
