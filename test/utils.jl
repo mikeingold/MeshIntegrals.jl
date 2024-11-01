1af6a60f38f79ea40c08ccb2a494b81caad22e57
Michael Ingold <mike.ingold@gmail.com>
2024-11-01 19:52:26 -0400

    Update tests

--- test/utils.jl
+++ test/utils.jl
@@ -10,17 +10,25 @@
     @test MeshIntegrals._units(p) == u"m"
 
     @testset "DifferentiationMethod" begin
+        # Test _has_analytical of instances
         bezier = BezierCurve([Point(t, sin(t), 0.0) for t in range(-π, π, length = 361)])
         sphere = Sphere(Point(0, 0, 0), 1.0)
-        @test MeshIntegrals.has_analytical(Meshes.BezierCurve) == true
-        @test MeshIntegrals.has_analytical(bezier) == true
-        @test MeshIntegrals.has_analytical(Meshes.Sphere) == false
-        @test MeshIntegrals.has_analytical(sphere) == false
+        @test MeshIntegrals._has_analytical(bezier) == true
+        @test MeshIntegrals._has_analytical(sphere) == false
 
-        @test MeshIntegrals.default_method(Meshes.BezierCurve) isa Analytical
-        @test MeshIntegrals.default_method(bezier) isa Analytical
-        @test MeshIntegrals.default_method(Meshes.Sphere) isa FiniteDifference
-        @test MeshIntegrals.default_method(sphere) isa FiniteDifference
+        # Test _has_analytical of types
+        @test MeshIntegrals._has_analytical(Meshes.BezierCurve) == true
+        @test MeshIntegrals._has_analytical(Meshes.Line) == true
+        @test MeshIntegrals._has_analytical(Meshes.Plane) == true
+        @test MeshIntegrals._has_analytical(Meshes.Ray) == true
+        @test MeshIntegrals._has_analytical(Meshes.Sphere) == false
+        @test MeshIntegrals._has_analytical(Meshes.Tetrahedron) == true
+        @test MeshIntegrals._has_analytical(Meshes.Triangle) == true
+
+        @test MeshIntegrals._default_method(Meshes.BezierCurve) isa Analytical
+        @test MeshIntegrals._default_method(bezier) isa Analytical
+        @test MeshIntegrals._default_method(Meshes.Sphere) isa FiniteDifference
+        @test MeshIntegrals._default_method(sphere) isa FiniteDifference
 
         @test FiniteDifference().ε ≈ 1e-6
     end
