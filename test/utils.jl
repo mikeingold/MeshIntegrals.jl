45b509c98e17cf916cf096f1c1fd43178825d60f
Michael Ingold <mike.ingold@gmail.com>
2024-11-01 21:22:03 -0400

    Split DifferentiationMethod into separate testitem

--- test/utils.jl
+++ test/utils.jl
@@ -9,27 +9,37 @@
     p = Point(1.0u"cm", 2.0u"mm", 3.0u"m")
     @test MeshIntegrals._units(p) == u"m"
 
-    @testset "DifferentiationMethod" begin
-        # Test _has_analytical of instances
-        bezier = BezierCurve([Point(t, sin(t), 0.0) for t in range(-π, π, length = 361)])
-        sphere = Sphere(Point(0, 0, 0), 1.0)
-        @test MeshIntegrals._has_analytical(bezier) == true
-        @test MeshIntegrals._has_analytical(sphere) == false
 
-        # Test _has_analytical of types
-        @test MeshIntegrals._has_analytical(Meshes.BezierCurve) == true
-        @test MeshIntegrals._has_analytical(Meshes.Line) == true
-        @test MeshIntegrals._has_analytical(Meshes.Plane) == true
-        @test MeshIntegrals._has_analytical(Meshes.Ray) == true
-        @test MeshIntegrals._has_analytical(Meshes.Sphere) == false
-        @test MeshIntegrals._has_analytical(Meshes.Tetrahedron) == true
-        @test MeshIntegrals._has_analytical(Meshes.Triangle) == true
+end
 
-        @test MeshIntegrals._default_method(Meshes.BezierCurve) isa Analytical
-        @test MeshIntegrals._default_method(bezier) isa Analytical
-        @test MeshIntegrals._default_method(Meshes.Sphere) isa FiniteDifference
-        @test MeshIntegrals._default_method(sphere) isa FiniteDifference
+@testitem "DifferentiationMethod" setup=[Setup] begin
+    using MeshIntegrals: _has_analytical, _default_method, _guarantee_analytical
 
-        @test FiniteDifference().ε ≈ 1e-6
-    end
+    # _has_analytical of instances
+    bezier = BezierCurve([Point(t, sin(t), 0.0) for t in range(-π, π, length = 361)])
+    @test _has_analytical(bezier) == true
+    sphere = Sphere(Point(0, 0, 0), 1.0)
+    @test _has_analytical(sphere) == false
+
+    # _has_analytical of types
+    @test _has_analytical(Meshes.BezierCurve) == true
+    @test _has_analytical(Meshes.Line) == true
+    @test _has_analytical(Meshes.Plane) == true
+    @test _has_analytical(Meshes.Ray) == true
+    @test _has_analytical(Meshes.Sphere) == false
+    @test _has_analytical(Meshes.Tetrahedron) == true
+    @test _has_analytical(Meshes.Triangle) == true
+
+    # _guarantee_analytical
+    @test _guarantee_analytical(Meshes.Line, Analytical()) === nothing
+    @test_throws "Analytical" _guarantee_analytical(Meshes.Line, FiniteDifference())
+
+    # _default_method
+    @test _default_method(Meshes.BezierCurve) isa Analytical
+    @test _default_method(bezier) isa Analytical
+    @test _default_method(Meshes.Sphere) isa FiniteDifference
+    @test _default_method(sphere) isa FiniteDifference
+
+    # FiniteDifference
+    @test FiniteDifference().ε ≈ 1e-6
 end
