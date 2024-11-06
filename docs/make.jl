d2fd7f4c2479286794d748113cfe30c71c673d9e
Michael Ingold <mike.ingold@gmail.com>
2024-11-05 20:01:09 -0500

    Remove errant character

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,8 @@ using MeshIntegrals
 # Dynamically set all files in subdirectories of the source directory to include all files in these subdirectories.
 # This way they don't need to be listed explicitly.
 SPECIALIZATIONS_FILES = joinpath.(Ref("specializations"),
-    readdir(joinpath(dirname(@__DIR__), "src",
-        "specializations")))
-r̄
+    readdir(joinpath(dirname(@__DIR__), "src", "specializations")))
+
 makedocs(
     sitename = "MeshIntegrals.jl",
     pages = [
