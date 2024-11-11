d92d543039efde5474e4fee3d8c4060be5e1e283
Michael Ingold <mike.ingold@gmail.com>
2024-11-10 23:16:01 -0500

    Remove obsolete code, update jacobian signature

--- src/specializations/BezierCurve.jl
+++ src/specializations/BezierCurve.jl
@@ -47,80 +47,19 @@ end
 #                              Parametric
 ################################################################################
 
-
 function _parametric(curve::Meshes.BezierCurve, t; alg::Meshes.BezierEvalMethod)
     return curve(t, alg)
 end
 
-
-
-#=
-function integral(
-        f::F,
-        curve::Meshes.BezierCurve,
-        rule::GaussLegendre;
-        diff_method::DM = _default_method(curve),
-        FP::Type{T} = Float64,
-        alg::Meshes.BezierEvalMethod = Meshes.Horner()
-) where {F <: Function, DM <: DifferentiationMethod, T <: AbstractFloat}
-    # Compute Gauss-Legendre nodes/weights for x in interval [-1,1]
-    xs, ws = _gausslegendre(FP, rule.n)
-
-    # Change of variables: x [-1,1] ↦ t [0,1]
-    t(x) = FP(1 // 2) * x + FP(1 // 2)
-    point(x) = curve(t(x), alg)
-    integrand(x) = f(point(x)) * differential(curve, (t(x),), diff_method)
-
-    # Integrate f along curve and apply domain-correction for [-1,1] ↦ [0, length]
-    return FP(1 // 2) * sum(w .* integrand(x) for (w, x) in zip(ws, xs))
-end
-
-function integral(
-        f::F,
-        curve::Meshes.BezierCurve,
-        rule::GaussKronrod;
-        diff_method::DM = _default_method(curve),
-        FP::Type{T} = Float64,
-        alg::Meshes.BezierEvalMethod = Meshes.Horner()
-) where {F <: Function, DM <: DifferentiationMethod, T <: AbstractFloat}
-    point(t) = curve(t, alg)
-    integrand(t) = f(point(t)) * differential(curve, (t,), diff_method)
-    return QuadGK.quadgk(integrand, zero(FP), one(FP); rule.kwargs...)[1]
-end
-
-function integral(
-        f::F,
-        curve::Meshes.BezierCurve,
-        rule::HAdaptiveCubature;
-        diff_method::DM = _default_method(curve),
-        FP::Type{T} = Float64,
-        alg::Meshes.BezierEvalMethod = Meshes.Horner()
-) where {F <: Function, DM <: DifferentiationMethod, T <: AbstractFloat}
-    point(t) = curve(t, alg)
-    integrand(ts) = f(point(only(ts))) * differential(curve, ts, diff_method)
-
-    # HCubature doesn't support functions that output Unitful Quantity types
-    # Establish the units that are output by f
-    testpoint_parametriccoord = zeros(FP, 1)
-    integrandunits = Unitful.unit.(integrand(testpoint_parametriccoord))
-    # Create a wrapper that returns only the value component in those units
-    uintegrand(ts) = Unitful.ustrip.(integrandunits, integrand(ts))
-    # Integrate only the unitless values
-    value = HCubature.hcubature(uintegrand, zeros(FP, 1), ones(FP, 1); rule.kwargs...)[1]
-
-    # Reapply units
-    return value .* integrandunits
-end
-=#
 ################################################################################
 #                               jacobian
 ################################################################################
 
 function jacobian(
         bz::Meshes.BezierCurve,
-        ts::V,
+        ts::Union{AbstractVector{T}, Tuple{T, Vararg{T}}},
         diff_method::Analytical
-) where {V <: Union{AbstractVector, Tuple}}
+) where {T <: AbstractFloat}
     t = only(ts)
     # Parameter t restricted to domain [0,1] by definition
     if t < 0 || t > 1
