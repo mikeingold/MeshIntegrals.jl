0ba43b8ffd51bf4db74bccec21e341dfc9526e7a
Michael Ingold <mike.ingold@gmail.com>
2024-11-10 22:16:46 -0500

    Test new version of Plane methods

--- src/specializations/Plane.jl
+++ src/specializations/Plane.jl
@@ -9,6 +9,31 @@
 ################################################################################
 
 function integral(
+    f::Function,
+    plane::Meshes.Plane,
+    rule::IntegrationRule;
+    kwargs...
+)
+    paramfunction(t) = _parametric(plane, t)
+    param_plane = _ParametricGeometry(paramfunction, 2)
+    return _integral(f, param_plane, rule; kwargs...)
+end
+
+################################################################################
+#                              Parametric
+################################################################################
+
+function _parametric(plane::Meshes.Plane, t1, t2)
+    f1(t) = t / (1 - t^2)
+    f2(t) = 2t - 1
+    f(t) = f1 ∘ f2
+    return plane(f(t1), f(t2))
+end
+
+
+#=
+
+function integral(
         f::F,
         plane::Meshes.Plane,
         rule::GaussLegendre;
@@ -102,3 +127,4 @@ end
 ################################################################################
 
 _has_analytical(::Type{T}) where {T <: Meshes.Plane} = true
+=#
