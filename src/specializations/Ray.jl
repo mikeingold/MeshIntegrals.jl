fad580d87fb1e7b1b7318a8da0dca95295ddf18f
Michael Ingold <mike.ingold@gmail.com>
2024-11-10 21:32:49 -0500

    Remove obsolete code

--- src/specializations/Ray.jl
+++ src/specializations/Ray.jl
@@ -1,12 +1,11 @@
-################################################################################
-#                   Specialized Methods for Ray
+############################################################################################
+#                              Specialized Methods for Ray
 #
 # Why Specialized?
-#   The Ray geometry is a special case, representing a ray, originating at a point
-#   and extending an infinite length in a particular direction. This requires
-#   a domain transformation mapping from the typical parametric region [0,1] to
-#   an infinite one (-∞,∞).
-################################################################################
+#   The Ray geometry is a special case, representing a ray, originating at a point and
+#   extending an infinite length in a particular direction. This requires a domain transform
+#   that maps from the typical parametric region [0,1] to an infinite one (-∞,∞).
+############################################################################################
 
 function integral(
     f::Function,
@@ -19,99 +18,9 @@ function integral(
     return _integral(f, param_ray, rule; kwargs...)
 end
 
-#=
-
-function integral(
-        f::F,
-        ray::Meshes.Ray,
-        rule::GaussLegendre;
-        diff_method::DM = Analytical(),
-        FP::Type{T} = Float64
-) where {F <: Function, DM <: DifferentiationMethod, T <: AbstractFloat}
-    _guarantee_analytical(Meshes.Ray, diff_method)
-
-    # Compute Gauss-Legendre nodes/weights for x in interval [-1,1]
-    xs, ws = _gausslegendre(FP, rule.n)
-
-    # Normalize the Ray s.t. ray(t) is distance t from origin point
-    ray = Meshes.Ray(ray.p, Meshes.unormalize(ray.v))
-
-    # Domain transformation: x ∈ [-1,1] ↦ t ∈ [0,∞)
-    t₁(x) = FP(1 / 2) * x + FP(1 / 2)
-    t₁′(x) = FP(1 / 2)
-    t₂(x) = x / (1 - x^2)
-    t₂′(x) = (1 + x^2) / (1 - x^2)^2
-    t = t₂ ∘ t₁
-    t′(x) = t₂′(t₁(x)) * t₁′(x)
-
-    # Integrate f along the Ray
-    domainunits = _units(ray(0))
-    integrand(x) = f(ray(t(x))) * t′(x) * domainunits
-    return sum(w .* integrand(x) for (w, x) in zip(ws, xs))
-end
-
-function integral(
-        f::F,
-        ray::Meshes.Ray,
-        rule::GaussKronrod;
-        diff_method::DM = Analytical(),
-        FP::Type{T} = Float64
-) where {F <: Function, DM <: DifferentiationMethod, T <: AbstractFloat}
-    _guarantee_analytical(Meshes.Ray, diff_method)
-
-    # Normalize the Ray s.t. ray(t) is distance t from origin point
-    ray = Meshes.Ray(ray.p, Meshes.unormalize(ray.v))
-
-    # Integrate f along the Ray
-    domainunits = _units(ray(0))
-    integrand(t) = f(ray(t)) * domainunits
-    return QuadGK.quadgk(integrand, zero(FP), FP(Inf); rule.kwargs...)[1]
-end
-
-function integral(
-        f::F,
-        ray::Meshes.Ray,
-        rule::HAdaptiveCubature;
-        diff_method::DM = Analytical(),
-        FP::Type{T} = Float64
-) where {F <: Function, DM <: DifferentiationMethod, T <: AbstractFloat}
-    _guarantee_analytical(Meshes.Ray, diff_method)
-
-    # Normalize the Ray s.t. ray(t) is distance t from origin point
-    ray = Meshes.Ray(ray.p, Meshes.unormalize(ray.v))
-
-    # Domain transformation: x ∈ [0,1] ↦ t ∈ [0,∞)
-    
-    t′(x) = (1 + x^2) / (1 - x^2)^2
-
-    # Integrate f along the Ray
-    domainunits = _units(ray(0))
-    integrand(x::AbstractVector) = f(ray(t(x[1]))) * t′(x[1]) * domainunits
-
-    # HCubature doesn't support functions that output Unitful Quantity types
-    # Establish the units that are output by f
-    testpoint_parametriccoord = zeros(FP, 1)
-    integrandunits = Unitful.unit.(integrand(testpoint_parametriccoord))
-    # Create a wrapper that returns only the value component in those units
-    uintegrand(uv) = Unitful.ustrip.(integrandunits, integrand(uv))
-    # Integrate only the unitless values
-    value = HCubature.hcubature(uintegrand, zeros(FP, 1), ones(FP, 1); rule.kwargs...)[1]
-
-    # Reapply units
-    return value .* integrandunits
-end
-
-################################################################################
-#                               jacobian
-################################################################################
-
-_has_analytical(::Type{T}) where {T <: Meshes.Ray} = true
-
-=#
-
-################################################################################
-#                              Parametric
-################################################################################
+############################################################################################
+#                                     Parametric
+############################################################################################
 
 function _parametric(ray::Meshes.Ray, t)
     f(t) = t / (1 - t^2)
