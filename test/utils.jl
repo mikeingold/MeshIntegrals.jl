876793b4e56c65fb80caad6cf91898354cdf304f
Mike Ingold <mike.ingold@gmail.com>
2024-11-29 12:15:46 -0500

    Update tests

--- test/utils.jl
+++ test/utils.jl
@@ -23,29 +23,29 @@ end
     using MeshIntegrals: _has_analytical, _default_method, _guarantee_analytical
 
     # _has_analytical of instances
-    bezier = BezierCurve([Point(t, sin(t), 0.0) for t in range(-π, π, length = 361)])
-    @test _has_analytical(bezier) == true
+    triangle = Triangle(Point(0, 0, 0), Point(0, 1, 0), Point(1, 0, 0))
+    @test _has_analytical(triangle) == true
     sphere = Sphere(Point(0, 0, 0), 1.0)
     @test _has_analytical(sphere) == false
 
+    # _default_method
+    @test _default_method(Meshes.Triangle) isa Analytical
+    @test _default_method(triangle) isa Analytical
+    @test _default_method(Meshes.Sphere) isa FiniteDifference
+    @test _default_method(sphere) isa FiniteDifference
+
     # _has_analytical of types
-    @test _has_analytical(Meshes.BezierCurve) == true
+    @test _has_analytical(Meshes.BezierCurve) == false
     @test _has_analytical(Meshes.Line) == true
     @test _has_analytical(Meshes.Plane) == true
     @test _has_analytical(Meshes.Ray) == true
     @test _has_analytical(Meshes.Sphere) == false
-    @test _has_analytical(Meshes.Tetrahedron) == true
+    @test _has_analytical(Meshes.Tetrahedron) == false
     @test _has_analytical(Meshes.Triangle) == true
 
     # _guarantee_analytical
     @test _guarantee_analytical(Meshes.Line, Analytical()) === nothing
     @test_throws "Analytical" _guarantee_analytical(Meshes.Line, FiniteDifference())
-
-    # _default_method
-    @test _default_method(Meshes.BezierCurve) isa Analytical
-    @test _default_method(bezier) isa Analytical
-    @test _default_method(Meshes.Sphere) isa FiniteDifference
-    @test _default_method(sphere) isa FiniteDifference
 
     # FiniteDifference
     @test FiniteDifference().ε ≈ 1e-6
